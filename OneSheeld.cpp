4d5edba899f914d91955612961fa445a10c0afb9
Mustafa Fayez <mustafa@integreight.com>
2013-09-26 12:55:07 +0200

    finished the main library structure closes #5

--- OneSheeld.cpp
+++ OneSheeld.cpp
@@ -20,5 +20,21 @@ void OneSheeldClass::write(char* data)
 }
 
 // private functions 
+void OneSheeldClass::write(char shieldID,char functionCommand, char* data)
+{
+  int dataLength;
+  Serial.write(STX); // send STX  to start the packet
+  Serial.write(shieldID);
+  Serial.write(functionCommand);
+  if (strlen(data)>140)
+  dataLength=140;
+  else 
+  dataLength=strlen(data);
+  for(int i=0; i<dataLength; i++) {
+    Serial.write(data[i]);        
+  }
+  
+  Serial.write(ETX); // send ETX  to start the packet
+}
 // instantiate object for users
 OneSheeldClass OneSheeld;
