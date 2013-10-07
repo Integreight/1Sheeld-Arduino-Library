af663ccc5e7dbb2ef0aa90a7fc67770b5ac0d159
Islam Mustafa <islam@integreight.com>
2013-10-07 15:09:18 +0200

    Added the structure of the accelerometer shield

---
+++ Accelerometer.cpp
@@ -0,0 +1,12 @@
+/*
+  AccelerometerShield.h - AccelerometerShield library
+  Copyright (C) 2013 Integreight Inc  All rights reserved.
+*/
+
+#include "OneSheeld.h"
+#include "AccelerometerShield.h"
+
+// public functions
+AccelerometerShieldClass::AccelerometerShieldClass() {}
+// instantiate object for users
+AccelerometerShieldClass Accelerometer;
---
+++ Accelerometer.h
@@ -0,0 +1,20 @@
+/*
+  AccelerometerShield.h - AccelerometerShield library
+  Copyright (C) 2013 Integreight Inc  All rights reserved.
+*/
+
+#ifndef AccelerometerShield_h
+#define AccelerometerShield_h
+
+// library defintions
+#define ACCELEROMETER_ID 0x33  // twitter shield id 
+
+class AccelerometerShieldClass
+{
+public:
+    AccelerometerShieldClass();
+private:
+};
+// instantiate object for users
+extern AccelerometerShieldClass Accelerometer;
+#endif
