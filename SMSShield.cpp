2af03e98fbcdaeee23123ea73a90821646fa2ae0
Islam Mustafa <islam@integreight.com>
2013-10-13 10:46:27 +0200

    Fixed an error with the sms shield

--- SMSShield.cpp
+++ SMSShield.cpp
@@ -11,8 +11,9 @@ SMSShieldClass::SMSShieldClass(){}
 void SMSShieldClass::send(char* number,char* text)
 {
 	char str[140];
+	strcpy (str, "");
 	strcat (str, number);
-	strcpy (str, "##");
+	strcat (str, "##");
 	strcat (str, text);
 	OneSheeld.write(SMS_ID,SEND_SMS,str);
 }
