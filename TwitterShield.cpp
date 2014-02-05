15eac14822cdec4a6e312a6d874956d64ce6d8e5
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-02-05 14:03:30 +0200

    Commented the Unused Array

--- TwitterShield.cpp
+++ TwitterShield.cpp
@@ -15,6 +15,7 @@ OneSheeld.sendPacket(TWITTER_ID,0,SEND_TWEET,1,new FunctionArg(strlen(data),(byt
 }
 void TwitterShieldClass::sendDirectMessage(char* username,char* message)
 {
+/*	
 int length = strlen(username)+ strlen(message);
 char data[length];
 for (int i=0;i<( strlen(username));i++)
@@ -27,6 +28,7 @@ for (int i=0;i<=strlen(message);i++)
 {
 data[i+strlen(username)+1]=message[i];
 }
+*/
 OneSheeld.sendPacket(TWITTER_ID,0,SEND_DIRECT_MESSAGE,2,new FunctionArg(strlen(username),(byte*)username),new FunctionArg(strlen(message),(byte*) message));
 
 }
