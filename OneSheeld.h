80634909c852e412897f35ef753d2ac7f872f5aa
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-02-05 14:09:23 +0200

    Added the new ID's and modified the ID's of the Old ones

--- OneSheeld.h
+++ OneSheeld.h
@@ -17,19 +17,30 @@
 #include "PhoneShield.h"
 #include "GPSShield.h"
 #include "SevSegShield.h"
+#include "SkypeShield.h"
+#include "MusicPlayerShield.h"
+#include "EmailShield.h"
+#include "FoursquareShield.h"
+#include "CameraShield.h"
 
 // shield ids
-#define TWITTER_ID       0x30 
-#define FACEBOOK_ID      0x31 
-#define LCD_SHIELD_ID    0x32
-#define KEYPAD_SHIELD_ID 0x33
-#define NOTIFICATION_ID  0x34 
-#define ACCELEROMETER_ID 0x35
-#define SMS_ID 0x36
-#define GAMEPAD_ID 0x37
-#define PHONE_ID 0x38
-#define GPS_ID 0x39
-#define SEV_SEG_ID 0x40
+#define TWITTER_ID       0x1A 
+#define FACEBOOK_ID      0x19 
+#define LCD_SHIELD_ID    0x17
+#define KEYPAD_SHIELD_ID 0x09
+#define NOTIFICATION_ID  0x06
+#define ACCELEROMETER_ID 0x0B
+#define SMS_ID 0x0D
+#define GAMEPAD_ID 0x0C
+#define PHONE_ID 0x20
+#define GPS_ID 0x1C
+#define SEV_SEG_ID 0x07
+#define SLIDER_ID 0x01
+#define SKYPE_ID 0x1F
+#define MUSIC_PLAYER_ID 0x1D
+#define EMAIL_ID 0x1E
+#define FOURSQUARE_ID 0x1B
+#define CAMERA_ID 0x15
 
 #define PACKET_SIZE 12
 
@@ -71,9 +82,9 @@ class OneSheeldClass
 public:
     OneSheeldClass();
     void begin(long baudRate);
-    void write(char shieldID, char functionCommand, char *data);
-    void write(char *data);
-    void write(char shieldID, char functionCommand, char *data, int length);
+    //void write(char shieldID, char functionCommand, char *data);
+    //void write(char *data);
+   // void write(char shieldID, char functionCommand, char *data, int length);
     void onSerialEvent(char);
    void sendPacket(byte shieldID, byte instanceID,byte functionCommand, byte argNo, ...);
     
