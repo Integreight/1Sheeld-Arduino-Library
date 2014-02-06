67bcd86081513fa947e958501ab6ccb155f039db
Islam Mustafa <islam@integreight.com>
2014-02-06 16:12:24 +0200

    Removed the message from Skype

--- SkypeShield.h
+++ SkypeShield.h
@@ -3,7 +3,6 @@
 
 #define CALL 0x01
 #define VIDEO_CALL 0x02
-#define MESSAGE 0x03
 
 class SkypeShieldClass 
 {
@@ -13,7 +12,6 @@ public:
 	SkypeShieldClass();
 	void call(char *username);
 	void videoCall(char *username);
-	void message(char * username,char * message);
 
 };
 
