82c27f60cedcb8bcc9218e9dd4da7b62c52f353d
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-02-09 15:50:03 +0200

    Modified (new message function)

--- SkypeShield.cpp
+++ SkypeShield.cpp
@@ -18,4 +18,9 @@ void SkypeShieldClass::videoCall(char * username)
 	OneSheeld.sendPacket(SKYPE_ID,0,VIDEO_CALL,1,new FunctionArg(strlen(username),(byte *)username));
 }
 
+void SkypeShieldClass::message(char* username,char* message)
+{
+	OneSheeld.sendPacket(SKYPE_ID,0,MESSAGE,2,new FunctionArg(strlen(username),(byte*)username),new FunctionArg(strlen(message),(byte*)message));
+}
+
 SkypeShieldClass Skype;
--- SkypeShield.h
+++ SkypeShield.h
@@ -1,9 +1,9 @@
 #ifndef SkypeShield_h
 #define SkypeShield_h
 
-#define CALL 0x01
-#define VIDEO_CALL 0x02
-
+#define CALL 		0x01
+#define VIDEO_CALL  0x02
+#define MESSAGE 	0x03
 class SkypeShieldClass 
 {
 private:
@@ -12,6 +12,7 @@ public:
 	SkypeShieldClass();
 	void call(char *username);
 	void videoCall(char *username);
+	void message(char * username, char *message);
 
 };
 
