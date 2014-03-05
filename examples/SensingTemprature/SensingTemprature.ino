e2f559bbd9f94c0eb40c54f48c0ea0602470e7f8
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-05 19:26:16 +0200

    Example on Temperature sensor

---
+++ examples/SensingTemprature/SensingTemprature.ino
@@ -0,0 +1,39 @@
+/*
+Example illustrates Sensing the temperature of 
+a room and Buzzing if the temperature exceeded a 
+certain value
+*/
+
+#include <OneSheeld.h>
+
+//reserving a varriable for the Temperature value and initiallizing it by zero
+char tempvalue=0;
+
+void setup () 
+{
+   
+  //Starting the UART communication on baud rate 57600
+   OneSheeld.begin(57600);
+   
+}
+
+//Always get temperature values from the Temperature Sensor of the mobile 
+void loop()
+{
+    //storing the temperature value
+    tempvalue=Temp.getDegree();
+    //Buzz if the Temperature exceeds 20 degrees in Celsuis 
+    if (tempvalue>=20)
+    {
+      //Buzzing
+      Buzzer.buzzOn();
+      //delay 10 seconds
+     delay(10000);
+      //Stop Buzzing  
+      Buzzer.buzzOff();
+    }
+    
+}
+
+
+
