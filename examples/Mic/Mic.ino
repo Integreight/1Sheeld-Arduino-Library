bcbe56511ff92e8faef2c498b4356bfef2e6476f
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-05 19:24:38 +0200

    Example on Mic Shield

---
+++ examples/Mic/Mic.ino
@@ -0,0 +1,25 @@
+/*
+Example illustrates the use of the Smart Phones Mic checking out the sound 
+in a certain room and when the sound exceeded a certain limit send a tweet 
+*/
+
+//including the OneSheeld Library 
+#include <OneSheeld.h>
+
+void setup ()
+{
+  //Start the UART communication on baudrate 57600
+  OneSheeld.begin();
+}
+
+
+void loop()
+{
+  //always get the value of the Mic and check if it exceeds a certain value 
+  if(Mic.getValue()>100)
+  {
+    //send a tweet 
+    Twitter.updateStatus("We Are JAMMING ;)"); 
+  }
+  
+}
