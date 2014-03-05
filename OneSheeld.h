271089373197e031dac288e91ef1bf44fb0259ca
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-05 19:23:11 +0200

    Refactored the #define Names of Shields

--- OneSheeld.h
+++ OneSheeld.h
@@ -41,40 +41,41 @@ typedef unsigned char byte;
 #include "OrientationSensorShield.h"
 #include "MagnetometerSensorShield.h"
 #include "LCDShield.h"
+#include "ClockShield.h"
 
 // shield ids
-#define TWITTER_ID       0x1A 
-#define FACEBOOK_ID      0x19 
-#define LCD_ID 		   	 0x17
-#define KEYPAD_SHIELD_ID 0x09
-#define NOTIFICATION_ID  0x06
-#define ACCELEROMETER_ID 0x0B
-#define SMS_ID 			 0x0D
-#define GAMEPAD_ID 		 0x0C
-#define PHONE_ID 		 0x20
-#define GPS_ID 			 0x1C
-#define SEV_SEG_ID 		 0x07
-#define SKYPE_ID 		 0x1F
-#define MUSIC_PLAYER_ID  0x1D
-#define EMAIL_ID 		 0x1E
-#define FOURSQUARE_ID 	 0x1B
-#define CAMERA_ID 		 0x15
-#define BUZZER_ID 		 0x08
-#define LED_ID 		 	 0x02
-#define SLIDER_ID 		 0x01
-#define PUSH_BUTTON_ID 	 0x03
+#define TWITTER_ID      	 0x1A 
+#define FACEBOOK_ID     	 0x19 
+#define LCD_ID 		   	 	 0x17
+#define KEYPAD_SHIELD_ID	 0x09
+#define NOTIFICATION_ID 	 0x06
+#define ACCELEROMETER_ID 	 0x0B
+#define SMS_ID 				 0x0D
+#define GAMEPAD_ID 			 0x0C
+#define PHONE_ID 			 0x20
+#define GPS_ID 				 0x1C
+#define SEV_SEG_ID 			 0x07
+#define SKYPE_ID 			 0x1F
+#define MUSIC_PLAYER_ID  	 0x1D
+#define EMAIL_ID 		  	 0x1E
+#define FOURSQUARE_ID 	 	 0x1B
+#define CAMERA_ID 		 	 0x15
+#define BUZZER_ID 		 	 0x08
+#define LED_ID 		 	 	 0x02
+#define SLIDER_ID 		 	 0x01
+#define PUSH_BUTTON_ID 	 	 0x03
 #define TOGGLE_BUTTON_ID 	 0x04
-#define FLASH_ID		 0x05
-#define PROXIMITY_ID	 0x13
-#define MIC_ID 			 0x18
-#define TEMPERATURE_ID	 0x12
-#define LIGHT_ID 		 0x10
-#define PRESSURE_ID		 0x11
-#define GRAVITY_ID 		 0x14
-#define GYROSCOPE_ID	 0x0E
-#define ORIENTATION_ID	 0x0F
-#define MAGNETOMETER_ID  0x0A
-
+#define FLASH_ID			 0x05
+#define PROXIMITY_ID	 	 0x13
+#define MIC_ID 				 0x18
+#define TEMPERATURE_ID		 0x12
+#define LIGHT_ID 			 0x10
+#define PRESSURE_ID			 0x11
+#define GRAVITY_ID 			 0x14
+#define GYROSCOPE_ID		 0x0E
+#define ORIENTATION_ID		 0x0F
+#define MAGNETOMETER_ID  	 0x0A
+#define CLOCK_ID		 	 0x21
 // start and end of the packet sent
 #define START_OF_FRAME  0xFF
 #define END_OF_FRAME 	0x00
