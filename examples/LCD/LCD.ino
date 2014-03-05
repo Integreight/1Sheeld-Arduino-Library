98bf2b3b72e21001697ba4fce98af56e0310537f
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-05 19:27:57 +0200

    Example on LCD Shield

--- examples/LCD/LCD.ino
+++ examples/LCD/LCD.ino
@@ -1,23 +1,57 @@
+/*
+Example illustrates the Liquid Crystal Display Shield
+*/
+
+//including the OneSheeld Library 
 #include <OneSheeld.h>
 
-void setup(){
-	// Initialize 1Sheeld
-	OneSheeld.begin();
-	pinMode(3,INPUT);
-}
+//reserving an array for the custom character 
+char  character[8]={0x00,0x0A,0x0A,0x0A,0x00,0x01,0x0E,0x00};  
 
-void loop(){
-  	if(digitalRead(3)==HIGH){
-  		// Print "Hello World!"
-		LCD.print("Hello, World!");
-		// Set the curson to the second row and the fourth column
-		LCD.setCursor(2,4);
-		// Print "From 1Sheeld"
-		LCD.print("From 1Sheeld");
-	}
+void setup () 
+{
+  //Starting the UART communication on baudrate 57600
+  OneSheeld.begin();
+  //set pin 13 as an in put 
+  pinMode(13,INPUT);  
 }
 
 
-
-
-
+void loop ()
+{
+  //always Check pin 13 if it is high (5V) display texts on LCD
+  if(digitalRead(13)==HIGH)
+  {
+   //begin the LCD cursor at the first row and first column
+   LCD.begin(1,1);
+   //blink the cursor and for 10 seconds
+   LCD.blink();
+   delay(10000);
+   //stop blinking
+   LCD.noBlink();
+   //write characters on LCD 
+   LCD.write('A');
+   LCD.write('B');
+   LCD.write('C');
+   //clear the LCD 
+   LCD.clear();
+   //print Hello World 
+   LCD.print("Hello World");
+   //set the cursor to begin writing from the second row and first column 
+   LCD.setCursor(2,1);
+   //print This is OneSheeld 
+   LCD.print("This is OneSheeld");
+   //print a custom character "a Smiley face" 
+   LCD.createChar(character);
+   //wait about 20 seconds  
+   delay(20000);
+   //print Bye Bye
+   LCD.print("BYE BYE");
+   //turn off the display
+   LCD.noDisplay();
+    
+  }
+  
+  
+   
+}
