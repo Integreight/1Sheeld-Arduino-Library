cb6806c012b8068e34be5733c49e19495f71e809
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-10 19:28:58 +0200

    Example Camera Shield

---
+++ examples/Camera/Camera.ino
@@ -0,0 +1,30 @@
+/*
+Example illustrates Sensing the Light and if its beyond a certain
+Limit take a Picture
+*/
+
+//Include OneSheeld Library 
+#include <OneSheeld.h>
+
+
+void setup ()
+{
+  //Start UART communication on buadrate 57600
+  OneSheeld.begin();
+  //Set the Flash of the Camera to be On while capturing 
+  Camera.setFlash(FLASH_ON);
+}
+
+
+void loop ()
+{
+  //Always get the value of the Light Sensor  
+  if(LightSensor.getValue()<100&&LightSensor.getValue()>0)
+  {
+    //Capture a picture
+    Camera.capture();
+    //Wait a second to handle the next capturing
+    delay(1000);
+  }
+  
+}
