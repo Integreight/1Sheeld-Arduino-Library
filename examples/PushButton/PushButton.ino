4f5394cc41e21cae88e35c40a6962662a4cf867f
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:17:30 +0200

    Refactored Push Button example

--- examples/PushButton/PushButton.ino
+++ examples/PushButton/PushButton.ino
@@ -2,35 +2,35 @@
 Example illustrates the use of the Push button in 1Sheeld
 */
 
-//including the OneSheeld Library 
+/*including the OneSheeld Library*/
 #include <OneSheeld.h>
 
-//reserving an integer varaiable to get the state of the button 
+/*reserving an integer varaiable to get the state of the button*/
 int state;
 
 void setup()
 {
-  //Starting the Uart communication on baudrate 57600
+  /*Starting the Uart communication on baudrate 57600*/
   OneSheeld.begin();
-  //set pin 13 as input
+  /*set pin 13 as input*/
   pinMode(13,INPUT);
-  //set pin A0 as output 
+  /*set pin A0 as output*/
   pinMode(A0,OUTPUT);
 }
 
 void loop()
 {
-  //listen pin 13 and check out it's value and put it in state
+  /*listen pin 13 and check out it's value and put it in state*/
   state=digitalRead(13);
-  //if the button is pressed ignite the light 
+  /*if the button is pressed ignite the light*/
   if(state==HIGH)
   {
     digitalWrite(A0,HIGH);
   }
-  //if not pressed nothing is done 
+  /*if not pressed nothing is done*/
   else
   {
-    digitalWrite(A0,LOW);`
+    digitalWrite(A0,LOW);
   }
 }
 
