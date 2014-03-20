c0b86c6cc425fff1a351c4c7ca83fa547fd53d70
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:02:31 +0200

    Example MusicPlayer Shield

---
+++ examples/Music_Player/Music_Player.ino
@@ -0,0 +1,44 @@
+/*
+Example Illustrates Controlling the Music Player using the timming Clock at a certain time 
+the Music Starts to Play
+*/
+
+/*Includig OneSheeld Library*/
+#include <OneSheeld.h>
+
+/*Reserve variables for the Clock timer*/
+byte seconds;
+byte minutes;
+byte hours;
+
+void setup ()
+{
+  /*Start UART communication on baudrate 57600*/
+  OneSheeld.begin();
+  /*Start the Clock Shield*/
+  Clock.begin();
+}
+
+void loop () 
+{
+  /*Always get the time to checkout on*/
+  seconds=Clock.getSeconds();
+  minutes=Clock.getMinutes();
+  hours=Clock.getHours();
+  
+  /*Play music at 8AM*/
+  if (hours==8 && minutes==0 && seconds==00)
+  {
+    /*Play on the music*/
+    MusicPlayer.play();
+    /*Set the volume*/
+    MusicPlayer.setVolume(5);
+  }
+  /*Stop music at 9AM*/
+  else if(hours==9 && minutes==0 && seconds==00)
+  {
+    /*Turn the music Off*/
+    MusicPlayer.stop();
+  }
+  
+}
