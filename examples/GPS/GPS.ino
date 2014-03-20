4877579a9bf758f3644900a5ffb84fb1b8f668cb
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:14:29 +0200

    Refactored GPS Example

--- examples/GPS/GPS.ino
+++ examples/GPS/GPS.ino
@@ -5,17 +5,17 @@ distance between the fixed coordinates and the new coordinates
 comming from the GPS of the Smart Phone
 */
 
-//including the OneSheeld Library
+/*including the OneSheeld Library*/
 #include <OneSheeld.h>
 
-//reserving a float variable to get the distance
+/*reserving a float variable to get the distance*/
 float distance;
 
 void setup() 
 {
-  //Starting the UART communication on baudrate 57600
+  /*Starting the UART communication on baudrate 57600*/
   OneSheeld.begin();
-  //set pin 13 as OUTPUT
+  /*set pin 13 as OUTPUT*/
   pinMode(13,OUTPUT);
 }
 
@@ -23,18 +23,18 @@ void setup()
 void loop()
 {
   
-  //always get the distance between the two coordinates getDistance(Fixed Longitude,Fixed Lattitude) 
+  /*always get the distance between the two coordinates getDistance(Fixed Longitude,Fixed Lattitude)*/
   distance=GPS.getDistance(30.0831008,31.3242943);
-   //always check if the new coordinate is in the range given by meters as 
-   //isInRange(Fixed Longitude,Fixed Lattitude,Range"in meters")   
+   /*always check if the new coordinate is in the range given by meters as*/ 
+   /*isInRange(Fixed Longitude,Fixed Lattitude,Range"in meters")*/   
   if(GPS.isInRange(30.0831008,31.3242943,100))
   {
-    //ignite the LED 
+    /*ignite the LED*/
     digitalWrite(13,HIGH);
   }
   else 
   {
-    //take off the LED 
+    /*take off the LED*/ 
     digitalWrite(13,LOW);
   }
 }
