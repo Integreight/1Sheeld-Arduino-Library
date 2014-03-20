22105d734a135b14e624644966ac535b54c50e0b
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:14:02 +0200

    Refactored Clock Example

--- examples/Clock/Clock.ino
+++ examples/Clock/Clock.ino
@@ -4,11 +4,11 @@ and time and when it is similar to a certain date send me SMS
 whishing me a HappYBirthdaY and update my status in FaceBook
 */
 
-//Include OneSheeld Library 
+/*Include OneSheeld Library*/
 #include <OneSheeld.h>
 
-//Reserving variables to get the time (hours-minutes-seconds)
-//and the Date (day-month)
+/*Reserving variables to get the time (hours-minutes-seconds)*/
+/*and the Date (day-month)*/
 byte hours;
 byte minutes;
 byte seconds;
@@ -18,15 +18,15 @@ short year;
 
 void setup ()
 {
-  //Start UART communication on baudrate 57600
+  /*Start UART communication on baudrate 57600*/
   OneSheeld.begin();
-  //Start the Clock Shield
+  /*Start the Clock Shield*/
   Clock.begin();
 }
 
 void loop () 
 {
-  //Always get the Date and Time 
+  /*Always get the Date and Time*/
   hours=Clock.getHours();
   minutes=Clock.getMinutes();
   seconds=Clock.getSeconds();
@@ -34,13 +34,13 @@ void loop ()
   month=Clock.getMonth();
   year=Clock.getYear();
   
-  //Check if the date is similar to my birthday date send me a 
-  //whishing birthday SMS at 3PM that day 
+  /*Check if the date is similar to my birthday date send me a*/
+  /*whishing birthday SMS at 3PM that day*/
   if(hours==15&&minutes==00&&seconds==00&&day==06&&month==02&&year==2014)
   {
-    //Send me SMS 
+    /*Send me SMS*/
     SMS.send("+201286077028","HappY BirTH daY :D from OneSheeld");
-    //Update my Facebook Status 
+    /*Update my Facebook Status*/ 
     Facebook.updateStatus("Today is My BirthDay :D ");
   }
 } 
