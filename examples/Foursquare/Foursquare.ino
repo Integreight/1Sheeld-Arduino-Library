d74fe2d441d3c613ffdba2463514efcd45ae556b
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 17:59:23 +0200

    Example Foursquare Shield

---
+++ examples/Foursquare/Foursquare.ino
@@ -0,0 +1,35 @@
+/*
+Example illustrates Checking in at the Great Pyramids in Giza (Egypt) using the Foursquare 
+Shield Application and the GPS Shield 
+*/
+
+/*Including OneSheeld Library*/
+#include <OneSheeld.h>
+
+/*Reserve a variable to check in once*/
+boolean checkedIn=true;
+
+void setup () 
+{
+  /*Start UART communication on baudrate 57600*/
+  OneSheeld.begin();
+}
+
+
+void loop () 
+{
+  /*Always get the GPS coordinates and if it is same as the coordinates of the Pyramids
+  check in*/
+  if (GPS.getLongitude()==31.134358 && GPS.getLattitude()==29.979175)
+  {
+    /*Check only once*/
+    if (checkedIn==true)
+    {
+      /*Check in at the Pyramids*/
+      Foursquare.checkIn("4b9f7a50f964a520422537e3","WoW what a great Place");
+      /*Reset the value of the variable*/
+      checkedIn=false;
+    }
+  }  
+}
+
