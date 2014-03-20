0598ccba44e9bd13486546ba59c7010c898d23ce
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:00:44 +0200

    Example GamePad

---
+++ examples/GamePad/GamePad.ino
@@ -0,0 +1,60 @@
+/*
+Example illustrates the GamePad Shield application by lighting some LEDs when pressing
+specific buttons or pressing multiple buttons at the same time
+*/
+
+/*Include OneSheeld Library*/
+#include <OneSheeld.h>
+
+
+
+void setup () 
+{
+  /*Start UART communication on baudrate 57600*/
+  OneSheeld.begin();
+  /*Set some pins OUTPUT*/
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  
+}
+
+
+void loop () 
+{
+  /*Always check on the values comming from the GamePad application*/
+  if (GamePad.isUpPressed())
+  {
+    /*Ignite LED on pin 13 when Up is Pressed*/
+    digitalWrite(13,HIGH);
+    digitalWrite(12,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+  }
+  else if(GamePad.isDownPressed())
+  {
+    /*Ignite LED on pin 12 when Down is Pressed*/
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+  }
+  else if (GamePad.isUpPressed()&&GamePad.isAPressed())
+  {
+    /*Ignite LED on pin 11 when Up and A is Pressed*/
+    digitalWrite(11,HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(10,LOW);
+  }
+  else if (GamePad.isDownPressed()&&GamePad.isBPressed())
+  {
+    /*Ignite LED on pin 10 when Down and B is Pressed*/
+    digitalWrite(10,HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(11,LOW);
+  }
+}
+
