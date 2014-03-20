2e3b374f1cf4a28c16e5f0877d34fdbf83388cfa
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-03-20 18:15:48 +0200

    Refactored Mic Example

--- examples/Mic/Mic.ino
+++ examples/Mic/Mic.ino
@@ -3,22 +3,22 @@ Example illustrates the use of the Smart Phones Mic checking out the sound
 in a certain room and when the sound exceeded a certain limit send a tweet 
 */
 
-//including the OneSheeld Library 
+/*including the OneSheeld Library*/
 #include <OneSheeld.h>
 
 void setup ()
 {
-  //Start the UART communication on baudrate 57600
+  /*Start the UART communication on baudrate 57600*/
   OneSheeld.begin();
 }
 
 
 void loop()
 {
-  //always get the value of the Mic and check if it exceeds a certain value 
+  /*always get the value of the Mic and check if it exceeds a certain value*/
   if(Mic.getValue()>100)
   {
-    //send a tweet 
+    /*send a tweet*/
     Twitter.updateStatus("We Are JAMMING ;)"); 
   }
   
