16b420f28941b6894b84eec5590c93b3c77f9f7a
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-04-07 17:09:22 +0200

    Refactored GamePad Shield

--- GamePadShield.h
+++ GamePadShield.h
@@ -3,14 +3,15 @@
 
 #define GAMEPAD_VALUE 0x01
 
-#define UP_BIT 4
-#define DOWN_BIT 5
-#define LEFT_BIT 6
-#define RIGHT_BIT 7
 #define A_BIT 0
 #define B_BIT 1
 #define C_BIT 2 
 #define D_BIT 3 
+#define UP_BIT 4
+#define DOWN_BIT 5
+#define LEFT_BIT 6
+#define RIGHT_BIT 7
+
 
 
 class GamePadShield
