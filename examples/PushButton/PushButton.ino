10b691edb6e8225bd9029eccc6cd10df221b8fcf
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-04-10 14:38:26 +0200

    Refactored the PushButton Example

--- examples/PushButton/PushButton.ino
+++ examples/PushButton/PushButton.ino
@@ -13,25 +13,23 @@ void setup()
   /*Starting the Uart communication on baudrate 57600*/
   OneSheeld.begin();
   /*set pin 13 as input*/
-  pinMode(13,INPUT);
-  /*set pin A0 as output*/
-  pinMode(A0,OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop()
 {
-  /*listen pin 13 and check out it's value and put it in state*/
-  state=digitalRead(13);
-  /*if the button is pressed ignite the light*/
-  if(state==HIGH)
+  /*Always check if the PushButton is pressed*/
+  if(PushButton.getValue()>0)
   {
-    digitalWrite(A0,HIGH);
+    /*Set the Led to HIGH*/
+    digitalWrite(13,HIGH);
   }
-  /*if not pressed nothing is done*/
   else
   {
-    digitalWrite(A0,LOW);
+    /*Set the Led to LOW*/
+    digitalWrite(13,LOW);
   }
+
 }
 
 
