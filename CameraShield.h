80f9af5f6f20913cce518f21eca3ffb436dfaa06
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-04 13:29:27 +0300

    Refactored the Literals of the Camera shield

--- CameraShield.h
+++ CameraShield.h
@@ -4,9 +4,9 @@
 
 #define REAR_CAPTURE 	0x01
 #define SET_FLASH 	0x02
-#define FLASH_OFF	0x00
-#define FLASH_ON    0x01
-#define FLASH_AUTO  0x02
+#define OFF	0x00
+#define ON    0x01
+#define AUTO  0x02
 #define FRONT_CAPTURE 0x03
 #define SET_QUALITY 0x04
 #define LOW_QUALITY 0x01
