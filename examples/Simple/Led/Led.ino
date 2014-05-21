b0d529dfb9cfe3ca36c83727dece9b7952bb00b8
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-21 21:10:56 +0300

    Added Simple LED Example

---
+++ examples/Simple/Led/Led.ino
@@ -0,0 +1,24 @@
+/*
+Example illustrates Blinking the LED in the Application 
+*/
+
+/*Include OneSheeld Library*/
+#include <OneSheeld.h>
+
+
+
+void setup ()
+{
+  /*Start Communication*/
+  OneSheeld.begin();
+}
+
+void loop () 
+{
+  /*Turn On the LED*/
+  LED.setHigh();
+  delay(1000);
+  /*Turn Off the LED*/
+  LED.setLow();
+  delay(1000);
+}
