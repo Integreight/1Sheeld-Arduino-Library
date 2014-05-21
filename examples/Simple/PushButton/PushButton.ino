4842b619a8ed59f95984b7b6bd6939b68dbd8f97
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-21 21:11:56 +0300

    Added Simple Push Button Example

---
+++ examples/Simple/PushButton/PushButton.ino
@@ -0,0 +1,36 @@
+/*
+Example illustrates the use of the Push button in 1Sheeld
+*/
+
+/*Include OneSheeld Library*/
+#include <OneSheeld.h>
+
+/*Reserving an integer varaiable to get the state of the button*/
+int state;
+
+void setup()
+{
+  /*Start Communication*/
+  OneSheeld.begin();
+  /*set pin 13 as input*/
+  pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+  /*Always check if the PushButton is pressed*/
+  if(PushButton.getValue()>0)
+  {
+    /*Set the Led to HIGH*/
+    digitalWrite(13,HIGH);
+  }
+  else
+  {
+    /*Set the Led to LOW*/
+    digitalWrite(13,LOW);
+  }
+
+}
+
+
+
