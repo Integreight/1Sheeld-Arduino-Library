6a4fae67f00e1b3015167442f0b90735e8cc2401
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-21 21:11:20 +0300

    Added Simple Notification Example

---
+++ examples/Simple/Notification/Notification.ino
@@ -0,0 +1,56 @@
+/*
+Example illustrates sending Notifications by pressing a button 
+*/
+
+/*Including OneSheeld Library*/
+#include <OneSheeld.h>
+
+/*Variable for last state of the button*/
+boolean lastState = LOW;
+/*Button on pin 12*/
+int buttonPin = 12;
+/*Led on pin 13*/
+int ledPin = 13;
+
+void setup () 
+{
+  /*Start Communication*/
+  OneSheeld.begin();
+  /*Set buttonPin as INPUT*/
+  pinMode(buttonPin,INPUT);
+  /*Set ledPin as OUTPUT*/
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop ()
+{
+  boolean buttonState = debounce(lastState);
+  /*Always check the button state*/
+  if(lastState == LOW && buttonState == HIGH)
+  {
+    /*Send Notification to your Phone*/
+    Notification.notifyPhone("we are Notifying the Phone");
+    /*Turn on the Led*/
+    digitalWrite(ledPin,HIGH);
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+  }
+  lastState = buttonState;
+}
+
+/*This Function prevent the hardware pushButton from bouncing*/ 
+boolean debounce (boolean lastState)
+{
+  boolean currentState = digitalRead(buttonPin);
+  
+  if(currentState != lastState)
+  {
+    delay(10);
+    currentState = digitalRead(buttonPin);
+  }
+  
+  return currentState;
+}
