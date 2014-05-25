a82b94d0224521f7909376db2310a4976b3d2e11
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:33:11 +0300

    Added Comment Slider

--- SliderShield.cpp
+++ SliderShield.cpp
@@ -1,32 +1,52 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          SliderShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "SliderShield.h"
 
+//Class Constructor
 SliderShield::SliderShield()
 {
 	value=0x00;
 	isCallBackAssigned=false;
 }
+//Getter
 byte SliderShield::getValue()
 {
 	return value;
 }
-
-
+//Phone Input Data Processing 
 void SliderShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId= OneSheeld.getFunctionId();
+
 	if(functionId==SLIDER_VALUE)
 		{
 			value= OneSheeld.getArgumentData(0)[0];
+			//Users Function Invocation
 			if(isCallBackAssigned)
 				(*changeCallBack)(value);
 		}
 }
-
+//Users Function Setter
 void SliderShield::setOnValueChange(void (*userFunction)(byte sliderValue))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
+//Instatntiating Object
 SliderShield Slider;
