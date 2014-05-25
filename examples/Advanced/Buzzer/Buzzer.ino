f08d14d38c095edaaff75691f4422507e025deed
Islam Mustafa <islam@integreight.com>
2014-05-25 03:43:36 +0200

    Refactored the buzzer example

--- examples/Advanced/Buzzer/Buzzer.ino
+++ examples/Advanced/Buzzer/Buzzer.ino
@@ -1,9 +1,14 @@
 /*
-Example Illustrates Buzzer buzz On each and every time you get near your 
-Smartphone using Proximity Sensor
+
+Buzzer Shield Example
+
+This example shows an application on 1Sheeld's buzzer shield.
+
+By using this example, you can buzz each and every time you get near your smartphone using the phone's proximity sensor.
+
 */
 
-/*Include OneSheeld Library*/
+/* Include 1Sheeld library. */
 #include <OneSheeld.h>
 
 void setup () 
@@ -15,15 +20,15 @@ void setup ()
 
 void loop ()
 {
-  /*Check out if something is near the Phone, buzz immediately*/
-  if (ProximitySensor.getValue()== 0)
+  /* Check out if something is near the phone, buzz immediately. */
+  if (ProximitySensor.getValue() == 0)
   {
-    /*Always buzz On when the something is near the Phone*/
+    /* Always buzz when the something is near the phone. */
     Buzzer.buzzOn();
   }
   else 
   {
-    /*Buzz off*/
+    /* Turn of the buzzer. */
     Buzzer.buzzOff();
   }
 }
