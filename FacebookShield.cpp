c66c877d97b2585206e0d56e3cc2a4ece1b271c8
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:27:48 +0300

    Added Comment Facebook

--- FacebookShield.cpp
+++ FacebookShield.cpp
@@ -1,21 +1,31 @@
 /*
-  FacebookShield.h - FacebookShield library
-  Copyright (C) 2013 Integreight Inc  All rights reserved.
+
+  Project:       1Sheeld Library 
+  File:          FacebookShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
 */
 
 #include "OneSheeld.h"
 #include "FacebookShield.h"
 
-// public functions
+//Sender
 void FacebookShieldClass::post(char* status)
 {
 	OneSheeld.sendPacket(FACEBOOK_ID,0,UPDATE_STATUS,1,new FunctionArg(strlen(status),(byte*)status));
 }
-
+//Sender
 void FacebookShieldClass::sendMessage(char * username,char * message)
 {
 	OneSheeld.sendPacket(FACEBOOK_ID,0,SEND_MESSAGE,2,new FunctionArg (strlen(username),(byte*)username),new FunctionArg(strlen(message),(byte*)message)); 
 }
 
-// instantiate object for users
+//Instantiating Object
 FacebookShieldClass Facebook;
--- FacebookShield.h
+++ FacebookShield.h
@@ -1,25 +1,38 @@
 /*
-  FacebookShield.h - FacebookShield library
-  Copyright (C) 2013 Integreight Inc  All rights reserved.
+
+  Project:       1Sheeld Library 
+  File:          FacebookShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
 */
+
 #ifndef FacebookShield_h
 #define FacebookShield_h
 
-// library defintions
+//Output Functions ID's
 #define UPDATE_STATUS 0x01  
 #define SEND_MESSAGE 0x02
 
 class FacebookShieldClass 
 {
 public:
-void post(char* );
-void sendMessage(char *,char* );
+	//Senders
+	void post(char* );
+	void sendMessage(char *,char* );
 
 private:
 
 
 
 };
-// instantiate object for users
+
+//Extern Object
 extern FacebookShieldClass Facebook;
 #endif
