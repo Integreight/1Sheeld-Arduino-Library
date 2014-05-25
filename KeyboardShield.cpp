9d4a50582a0de89d6ac82026aedef6974c6f14d4
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:29:18 +0300

    Added Comment Keyboard

--- KeyboardShield.cpp
+++ KeyboardShield.cpp
@@ -1,36 +1,50 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          KeyboardShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
 
 #include "OneSheeld.h"
 #include "KeyboardShield.h"
 
-
+//Class Constructor
  KeyboardShield::KeyboardShield()
 {
 	character=NULL;
 	isCallBackAssigned=false;
 }
 
-
+//Getter 
 char KeyboardShield::getCharacter()
 {
 	return character;
 }
-
+//Keyboard Input Data Processing 
 void KeyboardShield::processData()
 {
-	/*Check the Function ID in the Frame*/
+	//Checking the Function-ID
 	byte functionId=OneSheeld.getFunctionId();
 	if (functionId==GET_CHAR)
 	{
-		/*Get the Character*/
+		
 		character=OneSheeld.getArgumentData(0)[0];
-
+		//Users Function Invocation
 		if(isCallBackAssigned)
 		{
 			(*buttonChangeCallBack)();
 		}
 	}
 }
-
+//Users Function Setter
 void KeyboardShield::setOnButtonChange(void (*userFunction)())
 {
 	buttonChangeCallBack=userFunction;
--- KeyboardShield.h
+++ KeyboardShield.h
@@ -1,29 +1,49 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          KeyboardShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
 
 #ifndef KEYBOARD_H_
 #define KEYBOARD_H_
 
+//Input Function ID 
 #define GET_CHAR	0x01
-
-
 
 
 class KeyboardShield
 {
 public:
+	//Constructor 
 	KeyboardShield();
+	//Getter 
 	char getCharacter();
+	//setOnChange Function for User Function
 	void setOnButtonChange(void (*)());
 private:
+	//Reserve Variable
+	char character;
+	//Boolean for the setOnChange Function
+	bool isCallBackAssigned;
+	//Process Input Data  
 	void processData();
 	void (*buttonChangeCallBack)();
-	bool isCallBackAssigned;
-	char character;
-
+	
 	friend class OneSheeldClass ;
 
 
 };
 
+//Extern Object
 extern KeyboardShield AsciiKeyboard;
 #endif
 
