f2fb8be1828b73c53e56af980ff17d896d0726cd
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:32:32 +0300

    Added Comment PushButton

--- PushButtonShield.cpp
+++ PushButtonShield.cpp
@@ -1,36 +1,54 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          PushButtonShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "PushButtonShield.h"
 
 
 
-
+//Class Constructor
 PushButtonShield::PushButtonShield()
 {
 	value=0x00;
 	isCallBackAssigned=false;
 }
-
+//Checker 
 bool PushButtonShield::isPressed()
 {
 	return !!value;
 }
-
+//Phone Input Data Processing 
 void PushButtonShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId =OneSheeld.getFunctionId();
+
 	if(functionId==PUSHBUTTON_VALUE)
 	{
 		value=OneSheeld.getArgumentData(0)[0];
+		//Users Function Invocation
 		if(isCallBackAssigned)
 			(*changeCallBack)(!!value);
 	}
 }
-
+//Users Function Setter
 void PushButtonShield::setOnButtonStatusChange(void (*userFunction)(bool pushButtonValue))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
-
+//Instatntiating Object
 PushButtonShield PushButton;
--- PushButtonShield.h
+++ PushButtonShield.h
@@ -1,23 +1,47 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          PushButtonShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef PushButtonShield_h
 #define PushButtonShield_h
 
+//Input Function ID
 #define PUSHBUTTON_VALUE 0x01
 
 
 class PushButtonShield
 {
 public:
+	//Constructor 
 	PushButtonShield();
+	//Checker
 	bool isPressed();
+	//setOnChange for Users Function
 	void setOnButtonStatusChange(void (*userFunction)(bool ));
 public:
-	void processData();
+	//Reserve Variable
 	byte value;
+	//Boolean For the setOnChange Function
 	bool isCallBackAssigned;
+	//Process Input Data 
+	void processData();
 	void (*changeCallBack)(bool);
+
 	friend class OneSheeldClass;
 	
 };
 
+//Extern Object
 extern PushButtonShield PushButton;
 #endif
