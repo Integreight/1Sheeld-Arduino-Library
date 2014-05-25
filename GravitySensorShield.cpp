356cb6e11f3ee0f848e04cf714a116f08d344a83
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:28:51 +0300

    Added Comment Gravity

--- GravitySensorShield.cpp
+++ GravitySensorShield.cpp
@@ -1,10 +1,23 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          GravitySensorShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "GravitySensorShield.h"
 
 
-
-
-
+//Class Constructor 
 GravitySensorShield::GravitySensorShield()
 {
 	valueX=0;
@@ -14,22 +27,22 @@ GravitySensorShield::GravitySensorShield()
 
 }
 
-
+//X-Axis getter function
 float GravitySensorShield::getX()
 {
 	return valueX;
 }
-
+//Y-Axis getter function
 float GravitySensorShield::getY()
 {
 	return valueY;
 }
-
+//Z-Axis getter function
 float GravitySensorShield::getZ()
 {
 	return valueZ;
 }
-
+//Gravity Input Data Processing 
 void GravitySensorShield::processData()
 {
 	
@@ -39,24 +52,28 @@ void GravitySensorShield::processData()
 		valueX=0;
 		valueY=0;
 		valueZ=0;
-		//getting the float of first 4bytes
+		
+		//Process X-Axis Value
 		getfloat.data[0]=OneSheeld.getArgumentData(0)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(0)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(0)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(0)[3];
 		valueX=getfloat.num;
-	    //getting the float of second 4bytes
+		
+		//Process Y-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(1)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(1)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(1)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(1)[3];
 		valueY=getfloat.num;
-	    //getting the float of third 4bytes
+		
+		//Process Z-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(2)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(2)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(2)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(2)[3];
 		valueZ=getfloat.num;
+
 		if(isCallBackAssigned)
 		{
 			(*changeCallBack)(valueX,valueY,valueZ);
@@ -65,11 +82,12 @@ void GravitySensorShield::processData()
 
 }
 
-
+//User Function Setter 
 void GravitySensorShield::setOnValueChange(void (*userFunction)(float valueX,float valueY,float valueZ))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
+//Instantiating Object
 GravitySensorShield GravitySensor ;
