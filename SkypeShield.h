c7b922e032914e4644f9b9494e404e5e8bfb4b7f
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:33:00 +0300

    Added Comment Skype

--- SkypeShield.h
+++ SkypeShield.h
@@ -1,17 +1,38 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          SkypeShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef SkypeShield_h
 #define SkypeShield_h
 
+//Output Function ID's
 #define CALL 		0x01
 #define VIDEO_CALL  0x02
 #define MESSAGE 	0x03
+
 class SkypeShieldClass 
 {
-private:
-	
+
 public:
+	//Setters 
 	void call(char *);
-	void videoCall(char *);
+	void videoCall(char *);	
+
+private:
+
 };
 
+//Extern Object
 extern SkypeShieldClass Skype;
 #endif
