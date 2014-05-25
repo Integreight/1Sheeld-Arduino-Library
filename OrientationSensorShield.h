d233b1b89639f251c7170a8bbf74f5b72c01d83a
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:31:34 +0300

    Added Comment Orientation

--- OrientationSensorShield.h
+++ OrientationSensorShield.h
@@ -1,31 +1,57 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          OrientationSensorShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef OrientationSensorShield_h
 #define OrientationSensorShield_h
 
-
+//Input Function ID
 #define ORIENTATION_VALUE 0x01
+
 
 class OrientationSensorShield
 {
 public:
+	//Constructor 
 	OrientationSensorShield();
+	//Getters
 	float getX();
 	float getY();
 	float getZ();
+	//setOnChange for Users Function
 	void setOnValueChange(void (*)(float,float,float));
 private:
+	//Reserve Variables
+	float valueX ,valueY,valueZ;
+	//Boolean for the setOnChange Function
+	bool isCallBackAssigned;
+	//Process Input Data  
 	void processData();
 	void (*changeCallBack)(float,float,float);
-	bool isCallBackAssigned;
-	float valueX ,valueY,valueZ;
-	union float_test
+	//Processing Float Numbers
+	union 
 	{
 		byte data[4];
 		float num;
 
 	} getfloat;
+
 	friend class OneSheeldClass;
 
 	
 };
+
+//Extern Object
 extern OrientationSensorShield OrientationSensor;
 #endif 
