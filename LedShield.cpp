a5df090a733db7aec73e2ee68fd643a3256bc55b
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:29:53 +0300

    Added Comment LED

--- LedShield.cpp
+++ LedShield.cpp
@@ -1,20 +1,36 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          LEDShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "LedShield.h"
 
+//Setter
 void LedShield::setLow()
 {
 	setValue(SET_LOW);
 }
-
+//Setter
 void LedShield::setHigh()
 {
 	setValue(SET_HIGH);
 }
-
+//Setter 
 void LedShield::setValue(byte data)
 {
 	OneSheeld.sendPacket(LED_ID,0,SET_VALUE,1,new FunctionArg(1,&data));
 }
 
-
+//Instantiating Object
 LedShield LED;
--- LedShield.h
+++ LedShield.h
@@ -1,6 +1,22 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          LEDShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef LedShield_h
 #define LedShield_h
 
+//Output Functions ID's
 #define SET_VALUE 0x01
 #define SET_LOW	  0x00
 #define SET_HIGH  0x01
@@ -8,14 +24,14 @@
 class LedShield
 {
 public:
+	//Setters
 	void setLow();
 	void setHigh();
 private:
 	void setValue(byte );
 
-	
-
 };
 
+//Extern Object
 extern LedShield LED;
 #endif
