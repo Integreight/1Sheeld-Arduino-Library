3e97bda74ad1b72dabba5db5747f5062628f7f1e
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:32:15 +0300

    Added Comment Proximity

--- ProximitySensorShield.cpp
+++ ProximitySensorShield.cpp
@@ -1,35 +1,52 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          ProximitySensorShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "ProximitySensorShield.h"
 
 
-
-
-
+//Class Constructor
 ProximitySensorShield::ProximitySensorShield()
 {
 	value=0x00;
 	isCallBackAssigned=false;
 }
-
+//Getter 
 byte ProximitySensorShield::getValue()
 {
 	return value;
 }
-
+//ProximitySensor Input Data Processing 
 void ProximitySensorShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId =OneSheeld.getFunctionId();
 	if(functionId==PROXIMITY_VALUE)
 	{
 		value=OneSheeld.getArgumentData(0)[0];
+		//Users Function Invocation
 		if (isCallBackAssigned)
 		(*changeCallBack)(value);
 	}
 }
-
+//Users Function Setter
 void ProximitySensorShield::setOnValueChange(void (*userFunction)(byte proximityValue))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
+
+//Instatntiating Object
 ProximitySensorShield ProximitySensor;
