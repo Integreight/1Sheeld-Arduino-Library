1e9c27d253d2050e407473f4e15899d5a2bc1a5e
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:30:50 +0300

    Added Comment Mic

--- MicShield.cpp
+++ MicShield.cpp
@@ -1,36 +1,55 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          MicShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "MicShield.h"
 
 
-
-
+//Class Constructor
 MicShield::MicShield()
 {
 	value=0x00;
 	isCallBackAssigned=false;
 }
-
+//Getter
 byte MicShield::getValue()
 {
 	return value;
 }
-
+//Mic Input Data processing 	
 void MicShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId =OneSheeld.getFunctionId();
 	if(functionId==MIC_VALUE)
 	{
 		value=0;
 		value=OneSheeld.getArgumentData(0)[0];
+
+		//Users Function Invocation
 		if(isCallBackAssigned)
 			(*changeCallBack)(value);
 	}
 }
 
+//Users Function Setter 
 void MicShield::setOnValueChange(void (*userFunction)(byte micValue))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
+//Instatntiating Object
 MicShield Mic;
--- MicShield.h
+++ MicShield.h
@@ -1,20 +1,45 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          MicShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef MicShield_h
 #define MicShield_h
 
+//Input Function ID 
 #define MIC_VALUE 0x01
 
 class MicShield
 {
 public:
+	//Constructor
 	MicShield();
+	//Getter
 	byte getValue();
+	//setOnChange for Users Function
 	void setOnValueChange(void (*)(byte));
 private:
+	//Reserve Variable
+	byte value;
+	//Boolean for the setOnChange Function
+	bool isCallBackAssigned;
+	//Process Input Data  
 	void processData();
 	void (*changeCallBack)(byte);
-	byte value;
-	bool isCallBackAssigned;
+	
 	friend class OneSheeldClass;
 };
+
+//Extern Object
 extern MicShield Mic;
 #endif
