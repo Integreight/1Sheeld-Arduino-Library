c74191c011c0bd3b67d70dce67bc1b74ada72c5c
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:30:39 +0300

    Added Comment Magnet

--- MagnetometerSensorShield.cpp
+++ MagnetometerSensorShield.cpp
@@ -1,10 +1,23 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          MagnetometerSensorShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "MagnetometerSensorShield.h"
 
 
-
-
-
+//Class Constructor
 MagnetometerSensorShield::MagnetometerSensorShield()
 {
 	valueX=0;
@@ -12,50 +25,53 @@ MagnetometerSensorShield::MagnetometerSensorShield()
 	valueZ=0;
 	isCallBackAssigned=false;
 }
-
-
+//X-Axis getter function
 float MagnetometerSensorShield::getX()
 {
 	return valueX;
 }
-
+//Y-Axis getter function
 float MagnetometerSensorShield::getY()
 {
 	return valueY;
 }
-
+//Z-Axis getter function
 float MagnetometerSensorShield::getZ()
 {
 	return valueZ;
 }
-
+//MagnetometerSensor Data processing 	
 void MagnetometerSensorShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId= OneSheeld.getFunctionId();
 	if(functionId==MAGNETOMETER_VALUE)
 	{
 		valueX=0;
 		valueY=0;
 		valueZ=0;
-		//getting the float of first 4bytes
+		//Process X-Axis Value
 		getfloat.data[0]=OneSheeld.getArgumentData(0)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(0)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(0)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(0)[3];
 		valueX=getfloat.num;
-	    //getting the float of second 4bytes
+
+	    //Process Y-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(1)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(1)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(1)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(1)[3];
 		valueY=getfloat.num;
-	    //getting the float of third 4bytes
+
+	    //Process Z-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(2)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(2)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(2)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(2)[3];
 		valueZ=getfloat.num;
 
+		//Users Function Invocation
 		if(isCallBackAssigned)
 		{
 			(*changeCallBack)(valueX,valueY,valueZ);
@@ -63,16 +79,17 @@ void MagnetometerSensorShield::processData()
 	}
 }
 
+//Users Function Setter 
 void MagnetometerSensorShield::setOnValueChange(void (*userFunction)(float valueX, float valueY ,float valueZ))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
-
+//Helper
 float MagnetometerSensorShield::getMagneticStrength()
 {
 	return sqrt((valueX*valueX)+(valueY*valueY)+(valueZ*valueZ));
 }
 
-
+//Instatntiating Object
 MagnetometerSensorShield MagnetometerSensor;
--- MagnetometerSensorShield.h
+++ MagnetometerSensorShield.h
@@ -1,34 +1,59 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          MagnetometerSensorShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef MagnetometerSensorShield_h
 #define MagnetometerSensorShield_h
 
+//Input Function ID 
 #define MAGNETOMETER_VALUE 0x01
-
-
 
 
 class MagnetometerSensorShield
 {
 public:
+	//Constructor 
 	MagnetometerSensorShield();
+	//Getters 
 	float getX();
 	float getY();
 	float getZ();
 	float getMagneticStrength();
+	//setOnChange for Users Function
 	void setOnValueChange(void (*)(float ,float ,float));
+
 private:
+	//Reserve Variables 
+	float valueX ,valueY,valueZ;
+	//Boolean for the setOnChange Function
+	bool isCallBackAssigned;
+	//Process Input Data 
 	void processData();
 	void (*changeCallBack)(float ,float ,float );
-	bool isCallBackAssigned;
-	float valueX ,valueY,valueZ;
-	union float_test
+	//Processing Float Numbers
+	union 
 	{
 		byte data[4];
 		float num;
 
 	} getfloat;
+
 	friend class OneSheeldClass;
 
 	
 };
+
+//Extern Object
 extern MagnetometerSensorShield MagnetometerSensor;
 #endif 
