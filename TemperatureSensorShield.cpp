903514e4a71054ee5ecae6c56098ac4e8ebf22b2
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:33:40 +0300

    Added Comment Temp

--- TemperatureSensorShield.cpp
+++ TemperatureSensorShield.cpp
@@ -1,34 +1,53 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          TemperatureSensorShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "TemperatureSensorShield.h"
 
 
-
+//Class Constructor
 TemperatureSensorShield::TemperatureSensorShield()
 {
 	value=0xff;
 	isCallBackAssigned=false;
 }
-
+//Getter 
 char TemperatureSensorShield::getValue()
 {
 	return value;
 }
-
+//TemperatureSensor Input Data Processing
 void TemperatureSensorShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId =OneSheeld.getFunctionId();
+
 	if(functionId==TEMPERATURE_VALUE)
 	{
 		value=OneSheeld.getArgumentData(0)[0];
+		//Users Function Invocation
 		if(isCallBackAssigned)
 			(*changeOnCallBack)(value);
 	}
 }
-
+//Users Function Setter
 void TemperatureSensorShield::setOnValueChange(void(*userFunction)(char temp))
 {
 	changeOnCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
+//Instatntiating Object
 TemperatureSensorShield TemperatureSensor;
