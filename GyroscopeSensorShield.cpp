5e623e5ff070e3a794f4b650fdacaa87d5ca898d
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:29:04 +0300

    Added Comment Gyroscope

--- GyroscopeSensorShield.cpp
+++ GyroscopeSensorShield.cpp
@@ -1,7 +1,22 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          GyroscopeSensorShield.cpp
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #include "OneSheeld.h"
 #include "GyroscopeSensorShield.h"
 
-
+//Class Constructor 
 GyroscopeSensorShield::GyroscopeSensorShield()
 {
 	valueX=0;
@@ -10,49 +25,53 @@ GyroscopeSensorShield::GyroscopeSensorShield()
 	isCallBackAssigned=false;
 }
 
-
+//X-Axis getter function
 float GyroscopeSensorShield::getX()
 {
 	return valueX;
 }
-
+//Y-Axis getter function
 float GyroscopeSensorShield::getY()
 {
 	return valueY;
 }
-
+//Z-Axis getter function
 float GyroscopeSensorShield::getZ()
 {
 	return valueZ;
 }
-
+//GyroscopeSensor Input Data processing 	
 void GyroscopeSensorShield::processData()
 {
+	//Checking the Function-ID
 	byte functionId =OneSheeld.getFunctionId();
 	if(functionId==GYROSCOPE_VALUE)
 	{
 		valueX=0;
 		valueY=0;
 		valueZ=0;
-		//getting the float of first 4bytes
+		
+		//Process X-Axis Value
 		getfloat.data[0]=OneSheeld.getArgumentData(0)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(0)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(0)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(0)[3];
 		valueX=getfloat.num;
-	    //getting the float of second 4bytes
+	    
+	    //Process Y-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(1)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(1)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(1)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(1)[3];
 		valueY=getfloat.num;
-	    //getting the float of third 4bytes
+	    
+	    //Process Z-Axis Value
 	    getfloat.data[0]=OneSheeld.getArgumentData(2)[0];
 		getfloat.data[1]=OneSheeld.getArgumentData(2)[1];
 		getfloat.data[2]=OneSheeld.getArgumentData(2)[2];
 		getfloat.data[3]=OneSheeld.getArgumentData(2)[3];
 		valueZ=getfloat.num;
-
+		//Users Function Invocation
 		if (isCallBackAssigned)
 		{
 			(*changeCallBack)(valueX,valueY,valueZ);
@@ -60,11 +79,12 @@ void GyroscopeSensorShield::processData()
     }
 }
 
+//Users Function Setter 
 void GyroscopeSensorShield::setOnValueChange(void (*userFunction)(float valueX, float valueY ,float valueZ))
 {
 	changeCallBack=userFunction;
 	isCallBackAssigned=true;
 }
 
-
+//Instatntiating Object
 GyroscopeSensorShield GyroscopeSensor ;
--- GyroscopeSensorShield.h
+++ GyroscopeSensorShield.h
@@ -1,30 +1,56 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          GyroscopeSensorShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef GyroscopeSensorShield_h
 #define GyroscopeSensorShield_h
 
+//Input Function ID 
 #define GYROSCOPE_VALUE 0x01
 
 class GyroscopeSensorShield
 {
 public:
+	//Constructor 
 	GyroscopeSensorShield();
+	//Getter Functions 
 	float getX();
 	float getY();
 	float getZ();
+	//setOnChange for Users Fucntion 
 	void setOnValueChange(void (*)(float,float,float));
 private:
+	//Reserve Variables
+	float valueX ,valueY,valueZ;
+	//Boolean for the setOnChange Function
+	bool isCallBackAssigned;
+	//Process Input Data 
 	void processData();
 	void (*changeCallBack)(float,float,float);
-	bool isCallBackAssigned;
-	float valueX ,valueY,valueZ;
-	union float_test
+	//Processing Float Numbers
+	union 
 	{
 		byte data[4];
 		float num;
 
 	} getfloat;
+
 	friend class OneSheeldClass;
 
 	
 };
+
+//Extern Object
 extern GyroscopeSensorShield GyroscopeSensor;
 #endif 
