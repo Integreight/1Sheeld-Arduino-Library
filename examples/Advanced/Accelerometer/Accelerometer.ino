c85cfd5017faca75a064670d599b959ea1d0e23f
Islam Mustafa <islam@integreight.com>
2014-05-25 03:43:19 +0200

    Refactored the Accelerometer example

--- examples/Advanced/Accelerometer/Accelerometer.ino
+++ examples/Advanced/Accelerometer/Accelerometer.ino
@@ -1,45 +1,51 @@
 /*
-Example illustrates grabbing your Smartphone Carefully! and Strike as if you are bowling
-so you can turn on the led on pin 13 
+
+Accelerometer Shield Example
+
+This example shows an application on 1Sheeld's accelerometer shield.
+
+By using this example, you can grab your smartphone and strike as if you are bowling so you can turn on the led on pin 13.
+
 */
 
-/*Inculde OneSheeld Library*/
+/* Include 1Sheeld library. */
 #include <OneSheeld.h>
 
-/*Accelerometer Values in 3D*/
+/* Variables for the accelerometer's values in 3 dimensions. */
 float x;
 float y;
 float z;
 
-/*Led on pin 13*/
+/* A name for the led on pin 13. */
 int ledPin = 13 ;
+int magnitudeThreshold
 
 void setup () 
 {
-  /*Start Communication*/
+  /* Start communication. */
   OneSheeld.begin();
-  /*Set pin ledPin OUTPUT*/
+  /* Set the led pin as output. */
   pinMode(ledPin,OUTPUT);
 }
 
 
 void loop () 
 {
-  /*Always get the values of the Accelerometer in the 3 Dimensions (X-Y-Z)*/
+  /* Always get the values of the accelerometer in the 3 dimensions (X, Y and Z). */
   x=AccelerometerSensor.getX();
   y=AccelerometerSensor.getY();
   z=AccelerometerSensor.getZ();
   
-  /*Check the Motion of the Smart phone when exceeded a limit turn the LED ON by calcuating the Magnitude
-  of the 3D vectors*/
+  /* Check the motion of the smartphone, if it exceeded a limit, turn the LED on. */
+  /* We do that by calcuating the magnitude of the 3D vectors. */
   if (sqrt((x*x)+(y*y)+(z*z))>31)
   {
-    /*Turn on the LED */
+    /* Turn on the LED. */
     digitalWrite(ledPin,HIGH);
   }
   else
   {
-    /*Turn off the LED*/
+    /* Turn off the LED. */
     digitalWrite(ledPin,LOW);
   }
 }
