eb567eb5e201a80957406a96d21b305aba041a92
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-25 11:27:32 +0300

    Added Comment Email

--- EmailShield.h
+++ EmailShield.h
@@ -1,18 +1,37 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          EmailShield.h
+                 
+  Version:       1.0
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.5
+
+*/
+
 #ifndef EmailShield_h
 #define EmailShield_h
 
-
+//Output Function ID
 #define SEND 0x01
 
 
 
 class EmailShieldClass 
 {
-private:
 
 public:
+	//Sender 
 	void send(char* ,char*,char* );
+
+private:
+
 };
 
+//Extern Object
 extern EmailShieldClass Email;
 #endif
