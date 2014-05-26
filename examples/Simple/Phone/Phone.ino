ca2c0fe6d21edc0ac772666fc81b16a59d7168f0
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-26 15:58:19 +0300

    Refactored Phone Simple Example using delay rather than debouncing

--- examples/Simple/Phone/Phone.ino
+++ examples/Simple/Phone/Phone.ino
@@ -5,8 +5,7 @@ Example illustrates pressing the Button to call your Friend
 /*Include OneSheeld Library*/
 #include <OneSheeld.h>
 
-/*Variable for last state of the button*/
-boolean lastState = LOW;
+
 /*Button on pin 12*/
 int buttonPin = 12;
 /*Led on pin 13*/
@@ -25,33 +24,17 @@ void setup ()
 
 void loop ()
 {
-  boolean buttonState = debounce(lastState);
   /*Always check the button state*/
-  if(lastState == LOW && buttonState == HIGH)
+  if(digitalRead(buttonPin)==HIGH)
   {
-      /*Call my friend*/
-      Phone.call("01119611432");
       /*Turn On the LED*/
       digitalWrite(ledPin,HIGH);
-      delay(200);
+      /*Call my friend*/
+      Phone.call("01119611432");
+      /*Delay for not sending excessive Calls*/
+      delay(300);
   }
+
   /*Turn Off the LED*/
   digitalWrite(ledPin,LOW);
-  lastState = buttonState;
 }
-
-
-/*This Function prevent the hardware pushButton from bouncing*/ 
-boolean debounce (boolean lastState)
-{
-  boolean currentState = digitalRead(buttonPin);
-  
-  if(currentState != lastState)
-  {
-    delay(10);
-    currentState = digitalRead(buttonPin);
-  }
-  
-  return currentState;
-}
-
