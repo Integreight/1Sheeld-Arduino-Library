ba94066563534cf302e50cccc066352f809af272
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-28 18:38:26 +0300

    Refactored Comments in Foursquare Library

--- FoursquareShield.cpp
+++ FoursquareShield.cpp
@@ -17,7 +17,7 @@
 #include "FoursquareShield.h"
 
 
-//Sender
+//Checking-In Sender
 void FoursquareShieldClass::checkIn(char* placeId, char* msg)
 {
 	OneSheeld.sendPacket(FOURSQUARE_ID,0,CHECK_IN,2,new FunctionArg(strlen(placeId),(byte*)placeId),new FunctionArg(strlen(msg),(byte*)msg));
