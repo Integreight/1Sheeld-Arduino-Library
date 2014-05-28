e0133518ec7a9f4e3870f4b1aea1009efbc38610
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-05-28 18:37:12 +0300

    Refactored Comments in Email Library

--- EmailShield.cpp
+++ EmailShield.cpp
@@ -16,7 +16,7 @@
 #include "OneSheeld.h"
 #include "EmailShield.h"
 
-//Sender
+//Email Sender
 void EmailShieldClass::send(char *email ,char* subject,char* message)
 {
 	OneSheeld.sendPacket(EMAIL_ID,0,SEND,3,new FunctionArg(strlen(email),(byte*)email),new FunctionArg(strlen(subject),(byte*)subject),new FunctionArg(strlen(message),(byte*)message));
