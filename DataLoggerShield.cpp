c0b123eb22d41d752fbf5b84970a51b813b96994
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2014-07-24 13:50:21 +0300

    Added Comments to the DataLogger shield and removed some unwanted functions

--- DataLoggerShield.cpp
+++ DataLoggerShield.cpp
@@ -1,40 +1,46 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+/*
 
+  Project:       1Sheeld Library 
+  File:          DataLogger.cpp
+                 
+  Version:       1.0.1
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.7
+
+*/
 #include "OneSheeld.h"
 #include "DataLoggerShield.h"
-#include "HardwareSerial.h"
 
 
-DataLoggerShield::DataLoggerShield()
-{
-
-}
-
+//Start Logging Data
 void DataLoggerShield::startLogging()
 {
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,START_LOG,0);
 }
-
+//Start Logging Data giving name to file created 
 void DataLoggerShield::startLogging(char * fileName)
 {
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,START_LOG,1,new FunctionArg(strlen(fileName),(byte *)fileName));
 }
 
+//Stop Logging Data
 void DataLoggerShield::stopLogging()
 {
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,STOP_LOG,0);
 }
 
-
+//Set File Format whether JSON or CSV
 void DataLoggerShield::setFileFormat(byte format)
 {
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,SET_FORMAT,1,new FunctionArg(1,&format));
 }
 
-
-void DataLoggerShield::add(int key,float value,int decimalAfterPoint)
+//Log Data
+void DataLoggerShield::log(int key,float value,int decimalAfterPoint)
 {
 	char floattostring[10]={0};
 	char inttostring [5]={0};
@@ -43,13 +49,13 @@ void DataLoggerShield::add(int key,float value,int decimalAfterPoint)
 
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,LOG_DATA,2,new FunctionArg(5,(byte *)inttostring),new FunctionArg(10,(byte*)floattostring));
 }
-
-void DataLoggerShield::add(char * key,float value,int decimalAfterPoint)
+//Log Data
+void DataLoggerShield::log(char * key,float value,int decimalAfterPoint)
 {
 	char floattostring[10]={0};
 	dtostrf(value,10,decimalAfterPoint,floattostring);
 
 	OneSheeld.sendPacket(DATA_LOGGER_ID,0,LOG_DATA,2,new FunctionArg(strlen(key),(byte *)key),new FunctionArg(10,(byte*)floattostring));
 }
-
+//Instantiating Object
 DataLoggerShield Logger;
--- DataLoggerShield.h
+++ DataLoggerShield.h
@@ -1,7 +1,21 @@
+/*
+
+  Project:       1Sheeld Library 
+  File:          DataLogger.h
+                 
+  Version:       1.0.1
+
+  Compiler:      Arduino avr-gcc 4.3.2
+
+  Author:        Integreight
+                 
+  Date:          2014.7
+
+*/
 #ifndef DataLogger_h
 #define DataLogger_h
 
-
+//Ouput Function ID's
 #define SET_FORMAT 	   0x01
 #define START_LOG  	   0x02
 #define STOP_LOG	   0x03
@@ -14,23 +28,21 @@
 class DataLoggerShield
 {
 public:
-	DataLoggerShield();
+	//Starters
 	void startLogging();
 	void startLogging(char *);
+	//Stopper
 	void stopLogging();
+	//Setter
 	void setFileFormat(byte);
-	void add(int ,float,int);
-	void add(char * ,float,int);
-	//void log();
-	//void graph(int ,int);
-	//void graph(char,int);
-	//void graph(char * ,int);
-	//void plot();
-
+	//Process 
+	void log(int ,float,int);
+	void log(char * ,float,int);
 private:
 
 
 };
 
+//Extern Object
 extern DataLoggerShield Logger;
 #endif
