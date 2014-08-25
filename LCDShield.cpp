36ef1c3f1c1695a77bb7b6743373e41e35b95a65
unknown <eslam.mohamed.fathy@gmail.com>
2014-08-25 18:22:50 +0300

    Refactored LCD Shield #define

--- LCDShield.cpp
+++ LCDShield.cpp
@@ -16,7 +16,7 @@
 #include "OneSheeld.h"
 #include "LCDShield.h"
 
-LCDShield::LCDShield():PrintClass(LCD_ID,WRITE,PRINT)
+LCDShield::LCDShield():PrintClass(LCD_ID,LCD_WRITE,LCD_PRINT)
 {}
 //Setter
 void LCDShield::begin()
@@ -30,67 +30,67 @@ void LCDShield::begin()
 //Clear Setter
 void LCDShield::clear()
 {
-	OneSheeld.sendPacket(LCD_ID,0,CLEAR,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_CLEAR,0);
 }
 //Home Setter
 void LCDShield::home()
 {
-	OneSheeld.sendPacket(LCD_ID,0,HOME,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_HOME,0);
 }
 //NoBlink Setter
 void LCDShield::noBlink()
 {
-	OneSheeld.sendPacket(LCD_ID,0,NOBLINK,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_NOBLINK,0);
 }
 //Blink Setter
 void LCDShield::blink()
 {
-	OneSheeld.sendPacket(LCD_ID,0,BLINK,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_BLINK,0);
 }
 //NoCursor Setter
 void LCDShield::noCursor()
 {
-	OneSheeld.sendPacket(LCD_ID,0,NOCURSOR,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_NOCURSOR,0);
 }
 //Display Cursor Setter
 void LCDShield::cursor()
 {
-	OneSheeld.sendPacket(LCD_ID,0,CURSOR,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_CURSOR,0);
 }
 //Scrolling Setter
 void LCDShield::scrollDisplayLeft()
 {
-	OneSheeld.sendPacket(LCD_ID,0,SCROLLLEFT,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_SCROLLLEFT,0);
 }
 //Scrolling Setter
 void LCDShield::scrollDisplayRight()
 {
-	OneSheeld.sendPacket(LCD_ID,0,SCROLLRIGHT,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_SCROLLRIGHT,0);
 }
 //left-Right Setter
 void LCDShield::leftToRight()
 {
-	OneSheeld.sendPacket(LCD_ID,0,LEFT_RIGHT,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_LEFT_RIGHT,0);
 }
 //Right-left Setter
 void LCDShield::rightToLeft()
 {
-	OneSheeld.sendPacket(LCD_ID,0,RIGHT_LEFT,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_RIGHT_LEFT,0);
 }
 //AutoScroll Setter
 void LCDShield::autoScroll()
 {
-	OneSheeld.sendPacket(LCD_ID,0,AUTOSCROLL,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_AUTOSCROLL,0);
 }
 //NoAutoScroll Setter
 void LCDShield::noAutoScroll()
 {
-	OneSheeld.sendPacket(LCD_ID,0,NOAUTOSCROLL,0);
+	OneSheeld.sendPacket(LCD_ID,0,LCD_NOAUTOSCROLL,0);
 }
 //Cursor Setter
 void LCDShield::setCursor(byte x ,byte y)
 {
-	OneSheeld.sendPacket(LCD_ID,0,SETCURSOR,2,new FunctionArg(1,&x),new FunctionArg(1,&y));
+	OneSheeld.sendPacket(LCD_ID,0,LCD_SETCURSOR,2,new FunctionArg(1,&x),new FunctionArg(1,&y));
 }
 
 //Instantiating Object
--- LCDShield.h
+++ LCDShield.h
@@ -18,23 +18,23 @@
 
 #include "OneSheeldPrint.h"
 
-//Output Functions ID's  
-#define BEGIN 0x01
-#define CLEAR 0x02
-#define HOME  0x03
-#define NOBLINK  0x04
-#define BLINK	  0x05
-#define NOCURSOR  0x06
-#define CURSOR    0x07
-#define SCROLLLEFT 0x08
-#define SCROLLRIGHT 0x09
-#define LEFT_RIGHT  0x0A
-#define RIGHT_LEFT  0x0B
-#define AUTOSCROLL	0x0C
-#define NOAUTOSCROLL 0x0D 
-#define SETCURSOR 	 0x0E
-#define WRITE 		 0x0F
-#define PRINT 		 0x11
+//Output Functions ID's
+#define LCD_BEGIN		 0x01  
+#define LCD_CLEAR 		 0x02
+#define LCD_HOME  		 0x03
+#define LCD_NOBLINK  	 0x04
+#define LCD_BLINK	  	 0x05
+#define LCD_NOCURSOR  	 0x06
+#define LCD_CURSOR    	 0x07
+#define LCD_SCROLLLEFT 	 0x08
+#define LCD_SCROLLRIGHT  0x09
+#define LCD_LEFT_RIGHT   0x0A
+#define LCD_RIGHT_LEFT   0x0B
+#define LCD_AUTOSCROLL	 0x0C
+#define LCD_NOAUTOSCROLL 0x0D 
+#define LCD_SETCURSOR 	 0x0E
+#define LCD_WRITE 		 0x0F
+#define LCD_PRINT 		 0x11
 
 class LCDShield : public PrintClass
 {
