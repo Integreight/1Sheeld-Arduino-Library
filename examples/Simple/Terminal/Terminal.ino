b5b847eb1d9e2867a88a07213785731f32395e28
unknown <eslam.mohamed.fathy@gmail.com>
2014-09-03 10:32:28 +0300

    Refactored Terminal shield simple example

--- examples/Simple/Terminal/Terminal.ino
+++ examples/Simple/Terminal/Terminal.ino
@@ -4,33 +4,44 @@ Terminal Shield Example
 
 This example shows an application on 1Sheeld's Terminal shield.
 
-By using this example, you can check the change of values in the 
-Accelerometer sensor.
+By using this example, you can Debug easily on Temperature sensor
+and check the values in runtime.
 
 */
 
 /* Include 1Sheeld library. */
 #include <OneSheeld.h>
 
+/* Fan on Pin 13. */
+int fanPin = 13;
+/* Reserve a byte for Temperature value. */
+byte tempValue = 0 ;
 
 void setup()
 {
   /* Start communication. */
   OneSheeld.begin();
+  /* Fan on pin 13. */
+  pinMode(fanPin,OUTPUT);
 }
 
 void loop ()
 {
-  /* Print AccelerometerX. */
-  Terminal.print("Acc X: ");
-  /* Print Accelerometer X-axis Value. */
-  Terminal.println(AccelerometerSensor.getX());
-  /* Print AccelerometerY. */
-  Terminal.print("Acc Y: ");
-  /* Print Accelerometer Y-axis Value. */
-  Terminal.println(AccelerometerSensor.getY());
-  /* Print AccelerometerZ. */
-  Terminal.print("Acc Z: ");
-  /* Print Accelerometer Z-axis Value. */ 
-  Terminal.println(AccelerometerSensor.getZ());
+  /* Always get TemperatureSensor value. */
+  tempValue = TemperatureSensor.getValue();
+  /* Check Temperature in the Room */
+  if(tempValue > 30)
+  {
+    /* Turn on the fan. */
+    digitalWrite(fanPin,HIGH);
+    /* Display the value on Terminal. */
+    Terminal.println(tempValue);
+  }
+  else
+  {
+    /* Turn off the fan. */
+    digitalWrite(fanPin,LOW);
+    /* Display the value on Terminal. */
+    Terminal.println(tempValue);
+  }
 }
