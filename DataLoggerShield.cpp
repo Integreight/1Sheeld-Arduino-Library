e17fb49a0672e4e6b2eae2f20673e89e4f2118d0
unknown <eslam.mohamed.fathy@gmail.com>
2014-09-03 10:24:13 +0300

    Refactored File Template version and release date

--- DataLoggerShield.cpp
+++ DataLoggerShield.cpp
@@ -3,13 +3,13 @@
   Project:       1Sheeld Library 
   File:          DataLogger.cpp
                  
-  Version:       1.0.1
+  Version:       1.2
 
   Compiler:      Arduino avr-gcc 4.3.2
 
   Author:        Integreight
                  
-  Date:          2014.7
+  Date:          2014.9
 
 */
 #include "OneSheeld.h"
