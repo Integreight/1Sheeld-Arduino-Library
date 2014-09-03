f75830949669942aff6b3665968bf8acf13fb3ef
unknown <eslam.mohamed.fathy@gmail.com>
2014-09-03 10:31:06 +0300

    Refacotred file template version and release date for VoiceRecognition

--- VoiceRecognitionShield.h
+++ VoiceRecognitionShield.h
@@ -3,13 +3,13 @@
   Project:       1Sheeld Library 
   File:          VoiceRecognitionShield.h
                  
-  Version:       3.0
+  Version:       1.2
 
   Compiler:      Arduino avr-gcc 4.3.2
 
   Author:        Integreight
                  
-  Date:          2014.5
+  Date:          2014.9
 
 */
 #ifndef	VoiceRecognitionShield_h
