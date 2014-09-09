40d20b97a3168e6b94076d15f4e2c9d8cc6889c8
unknown <eslam.mohamed.fathy@gmail.com>
2014-09-10 01:19:45 +0300

    Refactored DataLogger Example

--- examples/Simple/DataLogger/DataLogger.ino
+++ examples/Simple/DataLogger/DataLogger.ino
@@ -5,7 +5,7 @@ Data-Logger Shield Example
 This example shows an application on 1Sheeld's data logger shield.
 
 By using this example, you can check the variation of the room's temperature 
-at your place by logging the data for 24 hours.
+at your place by logging the data for 12 hours.
 
 */
 
@@ -13,39 +13,59 @@ at your place by logging the data for 24 hours.
 #include <OneSheeld.h>
 
 /* Variable for the hours. */
-int hour = 0;
-/* Boolean to stop logging. */
-bool stopped = false;
+int counter = 0;
+/* Button on pin 12. */
+int buttonPin = 12;
+/* Boolean to start logging. */ 
+bool startFlag = false;
 
 void setup() 
 {
   /* Start communication. */
   OneSheeld.begin();
   /* Start logger shield. */
-  Logger.start();
-  /* Start clock shield. */
-  Clock.begin();
+  Logger.stop();
+  /* Set buttonPin as input. */
+  pinMode(buttonPin,INPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
 }
 
 void loop()
 {
-  /* Always get hours. */
-  hour = Clock.getHours();
-  /* Add temperature values as a column in the file. */
-  Logger.add("Temperature in celsuis",TemperatureSensor.getValue());
-  /* Log the row in the file. */
-  Logger.log();
-  /* Once the day ends stop logging and generate the file. */
-  if(hour==0 && !stopped)
+  if(digitalRead(buttonPin)== HIGH)
+  {
+    /* First insure to stop last logged data. */
+    Logger.stop();
+    /* Set a Delay. */
+    delay(500);
+    /* Start Logging. */
+    Logger.start();
+    /* Set startFlag. */
+    startFlag = true;
+    
+  }
+
+  /* Check Logging started. */
+  if(startFlag)
+  {
+    /* Add temperature values as a column in the file. */
+    Logger.add("Decibles",Mic.getValue());
+    /* Log the row in the file. */
+    Logger.log();  
+    /* Delay 1000 msec. */
+    delay(1000);
+    /* Increment counter. */
+    counter++;
+
+  }
+  
+  /* Once the half day ends stop logging and generate the file(12 hours=12*60*60 seconds ). */
+  if(counter==12*60*60)
   {
     /* Stop logging. */
     Logger.stop();
-    /* Set stopped boolean to true. */
-    stopped=true;
-  }
-  else
-  {
-    /* Reset stopped boolean to false. */
-    stopped=false;
+    /* Reset counter. */
+    counter=0;
   }
 }
