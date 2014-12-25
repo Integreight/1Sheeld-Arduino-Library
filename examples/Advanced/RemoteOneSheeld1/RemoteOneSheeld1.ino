bdf0a834d35289015578bb238eaab74bb549a0b1
Islam Mustafa <islam@integreight.com>
2014-12-25 15:10:31 +0200

    Refactored RemoteOneSheeld example

--- examples/Advanced/RemoteOneSheeld1/RemoteOneSheeld1.ino
+++ examples/Advanced/RemoteOneSheeld1/RemoteOneSheeld1.ino
@@ -1,60 +1,61 @@
 /*
 
- RemoteOneSheeld Example
+RemoteOneSheeld Example #1
 
- This example shows an application on 1Sheeld's Remote Feature.
+This example shows an application on remote 1Sheeld's feature.
+
+By using this example you can hook up a toggle button to your 1Sheeld system and control an
+LED on another 1Sheeld system through the internet. 
+
+*/
  
- By using this example you can hook up a toggle button to your 1Sheeld system and control an
- Led on another 1Sheeld system exists around the globe. 
+/* Include 1Sheeld library. */
+#include <OneSheeld.h>
  
- */
+/*
+
+Instantiate an object from RemoteOneSheeld Library and insert the unique address of the 
+remote system given you in the application about dialog.
+
+*/
+
+RemoteOneSheeld usaSheeld ("-----REMOTE-1SHEELD-ADDRESS-----");
+
+int remotePin = 11;
+int ledPin = 13;
+
+void setup()
+{
+  /* Start communication. */
+  OneSheeld.begin();
+  /* Listen to the remote 1Sheeld. */
+  OneSheeld.listenToRemoteOneSheeld(&usaSheeld);
+  /* Subscribe to a certain pin on the remote 1Sheeld system in USA for instance. */
+  usaSheeld.subscribeToChanges(remotePin);
+  /* Set a certain function to be invoked once the remote system responds with the state 
+    of the pin. 
+  */
+  usaSheeld.setOnSubscribeOrDigitalChange(&toggleLed);
+  /* Set an LED to pin 13. */ 
+  pinMode(ledPin,OUTPUT);
  
- /* Include 1Sheeld library. */
- #include <OneSheeld.h>
- 
- /* Insatntiate an Object from RemoteOneSheeld Library and insert the unique address of the 
- remote system given you in the application.
-    You can get the remote address from the application by pressing Menu >> About , and you 
- can share the address Id with anyone by pressing shareId. 
- */
- RemoteOneSheeld usaSheeld ("place the remote shared ID here");
- 
- int remotePin = 11;
- int ledPin = 13;
- 
- void setup()
- {
-   /* Start communication. */
-   OneSheeld.begin();
-   /* Listen to the remote 1Sheeld. */
-   OneSheeld.listenToRemoteOneSheeld(&usaSheeld);
-   /* Subscribe to a certain pin on the remote 1Sheeld system in USA. */
-   usaSheeld.subscribeToChanges(remotePin);
-   /* Set a certain function to be invoked once the remote system responds with the state 
-      of the pin. 
-   */
-   usaSheeld.setOnSubscribeOrDigitalChange(&toggleLed);
-   /* Set an Led to pin 13. */ 
-   pinMode(ledPin,OUTPUT);
-   
- }
- 
- void loop()
- {
-   /* Say any sentences to be said out loud there in the USA smartphone. */
-   if(VoiceRecognition.isNewCommandReceived())
+}
+
+void loop()
+{
+ /* Send any sentences to be said out loud in the other smartphone. */
+  if(VoiceRecognition.isNewCommandReceived())
   {
     /* Send the Command to the USA 1Sheeld system. */
     usaSheeld.sendMessage("USA",VoiceRecognition.getLastCommand());  
   }
-  
- }
- 
- /* Function to Toggle the LED according to the state comming from the remote 1Sheeld 
-    system.
- */
- void toggleLed(byte pinNumber , bool remotePinValue)
- {
-    /* Turn on the LED. */
-    digitalWrite(ledPin,remotePinValue); 
- }
+
+}
+
+
+/* Function to Toggle the LED according to the state coming from the remote 1Sheeld system. */
+void toggleLed(byte pinNumber , bool remotePinValue)
+{
+  /* Turn on the LED. */
+  digitalWrite(ledPin,remotePinValue); 
+}
--- examples/Advanced/RemoteOneSheeld2/RemoteOneSheeld2.ino
+++ examples/Advanced/RemoteOneSheeld2/RemoteOneSheeld2.ino
@@ -1,34 +1,37 @@
 /*
  
- RemoteOneSheeld Example #2
- 
- This example shows an application on 1Sheel's Remote Feature.
- 
- By burning this example on the the 1Sheels system you can receive the voice command from 
- the remote 1Sheeld system and say it loudly by the TextToSpeech shield in 1Sheeld app.
- 
- Note : you have to burn Remote_example1 on the remote system1 to complete this example.
+RemoteOneSheeld Example #2
+
+This example shows an application on remote 1Sheeld's feature.
+
+By uploading this example on the 1Sheeld's system you can receive voice commands from 
+another 1Sheeld system and say it loudly by the text to speech shield in 1Sheeld app.
+
+Note : you have to burn RemoteOneSheeld1 on the other 1Sheeld for this example to work.
+
 */
 
 /* Include 1Sheeld Library. */
 #include <OneSheeld.h>
-
 
 void setup()
 {
   /* Start communication. */
   OneSheeld.begin();
   /* Set a certain function to be invoked once the remote system get a certain command. */
-  OneSheeld.setOnStringMessage(&sayItLoud);
+  OneSheeld.setOnNewMessage(&sayItLoud);
 }
 
 void loop()
 {}
 
 /* Function to say the command out loud using the TextToSpeech shield. */
-void sayItLoud(String remoteAddress ,String key , String voiceCommand)
+void sayItLoud(char * remoteAddress ,char * key , char * voiceCommand)
 {
-  TextToSpeech.say(voiceCommand);
+  if(!strcmp(key,"USA"))
+  {
+   TextToSpeech.say(voiceCommand);
+  }
 }
 
 
