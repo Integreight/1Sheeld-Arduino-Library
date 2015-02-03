0c5c55d15f6a9ec86343f8c7704b52e238d48f3d
Eslam Ali <eali@integreight.com>
2015-02-03 15:59:43 +0200

    HttpResponse in New File

---
+++ HttpResponse.cpp
@@ -0,0 +1,110 @@
+#include "OneSheeld.h"
+#include "InternetShield.h"
+
+
+HttpResponse::HttpResponse()
+{
+	isDisposedTriggered=false;
+	bytes=NULL;
+}
+
+int HttpResponse::getStatusCode()
+{
+	return statusCode;
+}
+
+int HttpResponse::getBytesCount()
+{
+	return bytesCount;
+}
+
+char * HttpResponse::getBytes()
+{
+	return bytes;
+}
+
+unsigned long HttpResponse::getTotalBytesCount()
+{
+	return totalBytesCount;
+}
+
+void HttpResponse::getTheseBytes(int start,int size)
+{
+	index=start;
+	byte startArray[2] ;
+  	startArray[1] = (start >> 8) & 0xFF;
+  	startArray[0] = start & 0xFF;
+
+  	byte sizeArray[2] ;
+  	sizeArray[1] = (size >> 8) & 0xFF;
+  	sizeArray[0] = size & 0xFF;
+
+  	byte reqId[2] ;
+  	reqId[1] = (requestId >> 8) & 0xFF;
+  	reqId[0] = requestId & 0xFF;
+	
+	OneSheeld.sendPacket(INTERNET_ID,0,RESPONSE_GET_NEXT_BYTES,3,new FunctionArg(sizeof(int),startArray),
+																 new FunctionArg(sizeof(int),sizeArray),
+																 new FunctionArg(sizeof(int),reqId));
+}
+
+void HttpResponse::getNextBytes(int size)
+{
+	getTheseBytes(index,size);
+}
+
+void HttpResponse::setOnNextResponseBytesUpdate(void (*userFunction)(HttpResponse &))
+{
+	callbacksRequested |= RESPONSE_GET_NEXT_RESPONSE_BIT;
+	getNextCallBack = userFunction;
+}
+
+void HttpResponse::setOnError(void (*userFunction)(int errorNumber))
+{
+	callbacksRequested |= RESPONSE_GET_ERROR_BIT;
+	getErrorCallBack = userFunction;
+}
+
+bool HttpResponse::isSentFully()
+{
+	return (totalBytesCount-index==0);
+}
+
+void HttpResponse::dispose()
+{
+	isDisposedTriggered = true;
+	if(!isInit && bytesCount!=0)
+	{
+		free(bytes);
+	}
+	isInit=false;
+	bytes=NULL;
+
+	byte reqId[2] ;
+  	reqId[1] = (requestId >> 8) & 0xFF;
+  	reqId[0] = requestId & 0xFF;
+	
+	OneSheeld.sendPacket(INTERNET_ID,0,RESPONSE_DISPOSE,1,new FunctionArg(sizeof(int),reqId));
+}
+
+bool HttpResponse::isDisposed()
+{
+	return isDisposedTriggered;
+}
+
+void HttpResponse::resetIndex(int x)
+{
+	index=x;
+}
+
+void HttpResponse::getHeader(char * headerName , void (*userFunction)(char * incomingheaderName ,char * IncomingHeaderValue))
+{
+	callbacksRequested |= RESPONSE_INPUT_GET_HEADER_BIT;
+	getHeaderCallBack = userFunction;
+	OneSheeld.sendPacket(INTERNET_ID,0,RESPONSE_INPUT_GET_HEADER,1,new FunctionArg(strlen(headerName),(byte *)headerName));
+}
+
+HttpResponse::~HttpResponse()
+{
+	if(isInit && bytesCount!=0)free(bytes);
+}
---
+++ HttpResponse.h
@@ -0,0 +1,63 @@
+#ifndef HttpResponse_h
+#define HttpResponse_h
+
+//Output Function ID's for Response class
+#define RESPONSE_DISPOSE					0x11
+#define RESPONSE_GET_NEXT_BYTES				0x12
+#define RESPONSE_GET_HEADER					0x13
+#define RESPONSE_INPUT_GET_HEADER			0x07
+//Input Function ID's for Response class
+#define	RESPONSE_GET_ERROR					0x08
+#define RESPONSE_GET_NEXT_RESPONSE			0x09
+//Bit Setters
+#define RESPONSE_INPUT_GET_HEADER_BIT		0x01
+#define	RESPONSE_GET_ERROR_BIT				0x02
+#define RESPONSE_GET_NEXT_RESPONSE_BIT		0x04
+
+
+class HttpResponse
+{
+public:
+	HttpResponse();
+	//Checkers
+	bool isSentFully();
+	bool isDisposed(); 
+	//Set On for userFunctions
+	void setOnNextResponseBytesUpdate(void (*)(HttpResponse&));
+	void setOnError(void (*)(int ));
+	//Setters
+	void dispose();
+	void resetIndex(int= 0);
+	//Getters
+	void getNextBytes(int=255);
+	void getTheseBytes(int ,int);
+	void getHeader(char * , void (*)(char * ,float ));
+	void getHeader(char * , void (*)(char * ,char * ));
+	// void getHeader(String , void (*)(String ,float ));
+	// void getHeader(String , void (*)(String ,String ));
+	int getStatusCode();
+	int getBytesCount();
+	unsigned long getTotalBytesCount();
+	char * getBytes();
+	String getBytesAsStrings();
+	~HttpResponse();
+private:
+	bool isInit;
+	bool isDisposedTriggered;
+	int statusCode;
+	unsigned long totalBytesCount;
+	char * bytes;
+	int bytesCount;
+	int requestId;
+	unsigned long index;
+	byte callbacksRequested;
+
+	void (*getNextCallBack)(HttpResponse &);
+	void (*getHeaderCallBack)(char * ,char *);
+	void (*getErrorCallBack)(int);
+
+friend class InternetShield;
+friend class HttpRequest;
+};
+
+#endif
