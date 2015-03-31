4d7dbc156344bd6bce3156b18c4d1a94ecc448f9
Islam Mustafa <islam@integreight.com>
2015-03-31 15:21:26 +0200

    Made the color shield work with 9 colors instead on one

--- ColorShield.cpp
+++ ColorShield.cpp
@@ -9,17 +9,39 @@ ColorShield::ColorShield() : ShieldParent(COLOR_ID)
 {
 	isNewColor = false;
 	colorCallBackInvoked =false;
+	fullOperation=false;
 }
 
 void ColorShield::setPalette(byte range)
 {
-	OneSheeld.sendPacket(COLOR_ID,0,COLOR_RANGE_ID,1,new FunctionArg(1,&range));
+	OneSheeld.sendPacket(COLOR_ID,0,COLOR_PALETTE_ID,1,new FunctionArg(1,&range));
 }
 
-Color ColorShield::getLastColor()
+void ColorShield::setCalculationMode(byte mode)
 {
-	isNewColor =false;
-	return colorInstance;
+	OneSheeld.sendPacket(COLOR_ID,0,COLOR_CALCULATION_MODE_ID,1,new FunctionArg(1,&mode));
+}
+
+void ColorShield::enableFullOperation()
+{
+	byte fullMode = 1;
+	OneSheeld.sendPacket(COLOR_ID,0,COLOR_FULL_OPERATION_ID,1,new FunctionArg(1,&fullMode));
+}
+
+void ColorShield::enableNormalOperation()
+{
+	byte normalMode = 0;
+	OneSheeld.sendPacket(COLOR_ID,0,COLOR_FULL_OPERATION_ID,1,new FunctionArg(1,&normalMode));
+}
+
+Color ColorShield::getLastColor(byte whichColor)
+{
+	if(whichColor>=9||!fullOperation)
+	{
+		whichColor=COLOR_CENTER_MIDDLE;
+		isNewColor =false;
+	}
+	return colorInstances[whichColor];
 }
 
 bool ColorShield::isNewColorReceived()
@@ -27,10 +49,27 @@ bool ColorShield::isNewColorReceived()
 	return isNewColor;
 }
 
+bool ColorShield::isFullOperation()
+{
+	return fullOperation;
+}
+
+void ColorShield::finishedReading()
+{
+	isNewColor=false;
+}
+
 void ColorShield::setOnNewColor(void (*userFunction)(Color))
 {
 	colorCallBack = userFunction;
 	colorCallBackInvoked = true;
+}
+
+
+void ColorShield::setOnNewColor(void (*userFunction)(Color,Color,Color,Color,Color,Color,Color,Color,Color))
+{
+	colorsCallBack = userFunction;
+	colorsCallBackInvoked = true;
 }
 
 void ColorShield::processData()
@@ -40,19 +79,44 @@ void ColorShield::processData()
 	if(functionId == COLOR_VALUE)
 	{
 		isNewColor = true;
+		fullOperation=false;
 
-		colorInstance.rgb = (unsigned long )(((unsigned long)getOneSheeldInstance().getArgumentData(0)[0])|
+		colorInstances[COLOR_CENTER_MIDDLE].rgb = (unsigned long )(((unsigned long)getOneSheeldInstance().getArgumentData(0)[0])|
 											 ((unsigned long)getOneSheeldInstance().getArgumentData(0)[1])<<8|
 											 ((unsigned long)getOneSheeldInstance().getArgumentData(0)[2])<<16);
 
-		colorInstance.hsb = (unsigned long)(((unsigned long)getOneSheeldInstance().getArgumentData(1)[3]<<24)|
+		colorInstances[COLOR_CENTER_MIDDLE].hsb = (unsigned long)(((unsigned long)getOneSheeldInstance().getArgumentData(1)[3]<<24)|
 											((unsigned long)getOneSheeldInstance().getArgumentData(1)[2]<<16)|
 											((unsigned long)getOneSheeldInstance().getArgumentData(1)[1]<<8)|
 											 (unsigned long)getOneSheeldInstance().getArgumentData(1)[0]);
 		
 		if(colorCallBackInvoked)
 		{
-			(*colorCallBack)(colorInstance);
+			(*colorCallBack)(colorInstances[COLOR_CENTER_MIDDLE]);
+		}
+	}
+	else if(functionId == ALL_COLORS_VALUE && getOneSheeldInstance().getArgumentNo()==18)
+	{
+		isNewColor = true;
+		fullOperation = true;
+
+		for(int i=0;i<getOneSheeldInstance().getArgumentNo();i+=2)
+		{
+			colorInstances[i].rgb = (unsigned long )(((unsigned long)getOneSheeldInstance().getArgumentData(i)[0])|
+											 ((unsigned long)getOneSheeldInstance().getArgumentData(i)[1])<<8|
+											 ((unsigned long)getOneSheeldInstance().getArgumentData(i)[2])<<16);
+
+			colorInstances[i].hsb = (unsigned long)(((unsigned long)getOneSheeldInstance().getArgumentData(i+1)[3]<<24)|
+												((unsigned long)getOneSheeldInstance().getArgumentData(i+1)[2]<<16)|
+												((unsigned long)getOneSheeldInstance().getArgumentData(i+1)[1]<<8)|
+												 (unsigned long)getOneSheeldInstance().getArgumentData(i+1)[0]);
+		}
+		
+		if(colorsCallBackInvoked)
+		{
+			(*colorsCallBack)(colorInstances[COLOR_UPPER_LEFT],colorInstances[COLOR_UPPER_MIDDLE],colorInstances[COLOR_UPPER_RIGHT],
+				colorInstances[COLOR_CENTER_LEFT],colorInstances[COLOR_CENTER_MIDDLE],colorInstances[COLOR_CENTER_RIGHT],
+				colorInstances[COLOR_LOWER_LEFT],colorInstances[COLOR_LOWER_MIDDLE],colorInstances[COLOR_LOWER_RIGHT]);
 		}
 	}
 }
--- ColorShield.h
+++ ColorShield.h
@@ -1,13 +1,40 @@
 #ifndef ColorShield_h
 #define ColorShield_h
 
-//Output Function ID
-#define COLOR_RANGE_ID	0x01
-//Input Function ID
-#define COLOR_VALUE	0x01
-
 #include "ShieldParent.h"
 
+//Output Function ID
+#define COLOR_PALETTE_ID			0x01
+#define COLOR_CALCULATION_MODE_ID	0x02
+#define COLOR_FULL_OPERATION_ID		0x03
+//Input Function ID
+#define COLOR_VALUE			0x01
+#define ALL_COLORS_VALUE 	0x02
+
+#define COLOR_UPPER_LEFT	0
+#define COLOR_UPPER_MIDDLE	1
+#define COLOR_UPPER_RIGHT	2
+#define COLOR_CENTER_LEFT	3
+#define COLOR_CENTER_MIDDLE	4
+#define COLOR_CENTER_RIGHT	5
+#define COLOR_LOWER_LEFT	6
+#define COLOR_LOWER_MIDDLE	7
+#define COLOR_LOWER_RIGHT	8
+
+#define MOST_DOMINANT_COLOR	0x01
+#define AVERAGE_COLOR		0x02
+
+#define _1_BIT_GRAYSCALE_PALETTE	1
+#define _2_BIT_GRAYSCALE_PALETTE	2
+#define _4_BIT_GRAYSCALE_PALETTE	3
+#define _8_BIT_GRAYSCALE_PALETTE	4
+#define _3_BIT_RGB_PALETTE			5
+#define _6_BIT_RGB_PALETTE			6
+#define _9_BIT_RGB_PALETTE			7
+#define _12_BIT_RGB_PALETTE			8
+#define _15_BIT_RGB_PALETTE			9
+#define _18_BIT_RGB_PALETTE			10
+#define _24_BIT_RGB_PALETTE			11
 
 struct Color{
 	unsigned long rgb;
@@ -67,15 +94,24 @@ class ColorShield : public ShieldParent
 {
 public:
 	ColorShield();
-	Color getLastColor();
+	Color getLastColor(byte=COLOR_CENTER_MIDDLE);
 	void setOnNewColor(void (*)(Color ));
+	void setOnNewColor(void (*)(Color,Color,Color,Color,Color,Color,Color,Color,Color));
 	bool isNewColorReceived();
 	void setPalette(byte);
+	void setCalculationMode(byte);
+	void enableFullOperation();
+	void enableNormalOperation();
+	bool isFullOperation();
+	void finishedReading();
 private:
 	bool isNewColor;
+	bool fullOperation;
 	bool colorCallBackInvoked;
-	Color colorInstance;
+	bool colorsCallBackInvoked;
+	Color colorInstances[9];
 	void (*colorCallBack)(Color);
+	void (*colorsCallBack)(Color,Color,Color,Color,Color,Color,Color,Color,Color);
 	void processData();
 };
 
