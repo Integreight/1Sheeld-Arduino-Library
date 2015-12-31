8d161959d2619a2a8a3498aa738d158dec7d8a21
Eslam Ali <eali@integreight.com>
2015-12-31 15:25:03 +0200

    Sending notification ID as integer value inside each query frame

--- IncomingNotification.cpp
+++ IncomingNotification.cpp
@@ -57,70 +57,65 @@ bool IncomingNotification::hasBigTitle()
 {
 	return !!(notificationType & (1<<BIG_TITLE_BIT));
 }
-bool IncomingNotification::hasBigTicker()
+bool IncomingNotification::hasTicker()
 {
-	return !!(notificationType & (1<<BIG_TICKER_BIT));
+	return !!(notificationType & (1<<TICKER_BIT));
 }
-void IncomingNotification::queryApp()
+void IncomingNotification::dismiss()
 {
-	int queryType = 0; 
-	queryType |= 0x100;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
-																		 ,new FunctionArg(sizeof(uint16_t),(byte*)&queryType));
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_DIMISS,1,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId));
+}
+void IncomingNotification::queryAppName()
+{
+	byte queryType = 0x01; 
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
+																		 ,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryTitle()
 {
-	byte queryType = 0; 
-	queryType |= 0x80;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x02;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryText()
 {
-	byte queryType = 0; 
-	queryType |= 0x40;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x03;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::querySubText()
 {
-	byte queryType = 0; 
-	queryType |= 0x20;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x04;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryInfoText()
 {
-	byte queryType = 0; 
-	queryType |= 0x10;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x05;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryBigText()
 {
-	byte queryType = 0; 
-	queryType |= 0x08;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x06;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryTextLines()
 {
-	byte queryType = 0; 
-	queryType |= 0x04;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x07;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
 void IncomingNotification::queryBigTitle()
 {
-	byte queryType = 0; 
-	queryType |= 0x02;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x08;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
-void IncomingNotification::queryBigTicker()
+void IncomingNotification::queryTicker()
 {
-	byte queryType = 0; 
-	queryType |= 0x01;
-	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(1,&notificationId)
+	byte queryType = 0x09;
+	OneSheeld.sendShieldFrame(NOTIFICATION_ID,0,NOTIFICATION_QUERY_DATA,2,new FunctionArg(sizeof(uint16_t),(byte*)&notificationId)
 																		,new FunctionArg(1,&queryType));
 }
