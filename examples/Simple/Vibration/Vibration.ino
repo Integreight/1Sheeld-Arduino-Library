2a80e7a9b39be3ae9169d08a6538cacde60fb675
Islam Mustafa <islam@integreight.com>
2016-01-14 14:05:22 +0200

    Refactored the 2 vibrations example and edited their comments

--- examples/Simple/Vibration/Vibration.ino
+++ examples/Simple/Vibration/Vibration.ino
@@ -4,7 +4,8 @@ Vibration Shield Example
 
 This example shows an application on 1Sheeld's vibration shield.
 
-By using this example, you can make your phone vibrate for a given amount of time.
+By using this example, you can make your phone vibrate for a given
+amount of time.
 
 OPTIONAL:
 To reduce the library compiled size and limit its memory usage, you
@@ -18,25 +19,25 @@ defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define.
 
 /* Include 1Sheeld library. */
 #include <OneSheeld.h>
+
 void setup() {
+  /* Start communication. */
   OneSheeld.begin();
 }
 
 void loop() {
-  /* Make a one time vibration that lasts for 1 second 
-     Notice that the time-unit is in Milliseconds*/
+  /* Vibrate once for 1 second. */
   Vibration.start(1000);
 
-  //Wait for 8 seconds
+  /* Wait for 8 seconds. */
   OneSheeld.delay(8000);
 
-  /* Make a vibration that lasts for 1 second
-     and repeat itself with a delay of 5 seconds*/
+  /* Vibrate for 1 second every 5 seconds. */
   Vibration.start(1000,5000);
 
-  //wait for 13 seconds
-  OneSheeld.delay(13000);
+  /* Wait for 12 seconds. */
+  OneSheeld.delay(12000);
 
-  //To stop any repetitive vibration
+  /* Stop the vibration. */
   Vibration.stop();
 }
