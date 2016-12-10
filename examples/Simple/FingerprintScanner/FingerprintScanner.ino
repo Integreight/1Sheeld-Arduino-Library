049b616c8573d84de0e0c2855cf2995c0b24c9fb
Eslam Ali <eali@integreight.com>
2016-12-10 08:40:27 +0200

    Added simple example to fingerpring shield

---
+++ examples/Simple/FingerprintScanner/FingerprintScanner.ino
@@ -0,0 +1,41 @@
+/*
+
+FingerprintScanner Shield Example
+
+This example shows an application on 1Sheeld's FingerprintScanner shield.
+
+By using this example, you can turn on an LED using your fingerprint.
+ 
+OPTIONAL:
+To reduce the library compiled size and limit its memory usage, you
+can specify which shields you want to include in your sketch by
+defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define. 
+
+*/
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_FINGERPRINT_SCANNER_SHIELD
+
+/* Include 1Sheeld library. */
+#include <OneSheeld.h>
+
+/* Set an LED on pin 13.*/
+int ledPin = 13;
+
+void setup() 
+{
+  /* Start communication. */
+  OneSheeld.begin();
+  /* LED pin mode is output. */
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop()
+{
+  if(FingerprintScanner.isVerified())
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(1000);
+    digitalWrite(ledPin,LOW);
+  }
+}
